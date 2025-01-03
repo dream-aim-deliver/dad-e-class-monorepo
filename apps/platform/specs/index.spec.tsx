75b972660be4d0ac6893a229ebeda6c8da57ab43
maany <imptodefeat@gmail.com>
2025-01-03 18:19:08 +0100

    platform: tests, add theme context to platform home page test

--- apps/platform/specs/index.spec.tsx
+++ apps/platform/specs/index.spec.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Page from '../src/app/page';
-
+import { ThemeProvider } from '@maany_shr/e-class-ui-kit/contexts';
 describe('Page', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Page />);
+    const { baseElement } = render(
+      <ThemeProvider>
+        <Page />
+      </ThemeProvider>,
+    );
     expect(baseElement).toBeTruthy();
   });
 });
