e19f64dd19debad7cdaafe531270e5f94f8d900f
maany <imptodefeat@gmail.com>
2025-01-03 15:51:44 +0100

    tailwind: configure sources

--- apps/platform/src/components/home.tsx
+++ apps/platform/src/components/home.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { Button } from '@maany_shr/e-class-ui-kit';
 import '@maany_shr/e-class-ui-kit/tailwind.css';
-// import { useTheme } from '@maany_shr/e-class-ui-kit/contexts';
+import { useTheme } from '@maany_shr/e-class-ui-kit/contexts';
 export interface HomeProps {
   serverSideParam: string;
 }
 
 export default function Home(props: HomeProps) {
-  // const {theme, setTheme} = useTheme();
+  const {theme, setTheme} = useTheme();
   return (
     <div className="flex flex-col items-center justify-center text-center">
       <p className="text-center">Cool things happen here</p>
@@ -15,8 +15,8 @@ export default function Home(props: HomeProps) {
         variant="primary"
         size="medium"
         onClick={() => {
-          console.log('Clicked')
-          // setTheme(theme === 'orange' ? 'blue' : 'orange')
+          console.log('Clicked: ' + theme);
+          setTheme(theme === 'orange' ? 'blue' : 'orange')
 
         }}
       >
