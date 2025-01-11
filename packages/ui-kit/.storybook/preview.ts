451e9742bd20bccc2d591aaa8daf29a5a2b78cc2
maany <imptodefeat@gmail.com>
2025-01-11 18:06:25 +0100

    chore(ui-kit): add theme-control (switch via class name)

--- packages/ui-kit/.storybook/preview.ts
+++ packages/ui-kit/.storybook/preview.ts
@@ -1,1 +1,13 @@
-import "../lib/assets/css/index.css";
+import '../lib/assets/css/index.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      orange: 'theme-orange',
+      blue: 'theme-blue',
+      darkorange: 'theme-dark-orange-shade',
+    },
+    defaultTheme: 'orange',
+  }),
+];
