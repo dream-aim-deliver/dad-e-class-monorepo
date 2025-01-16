7ce950a3c5343344469cc78905caccec35eda9f0
divyanshu <divyanshugautam321@gmail.com>
2025-01-16 17:19:38 +0530

    changes

--- apps/platform/src/app/page.tsx
+++ apps/platform/src/app/page.tsx
@@ -7,9 +7,11 @@ export default function Index() {
    * Note: The corresponding styles are in the ./index.tailwind file.
    */
   const serverSideParam = "Hello from server";
+  //changes
   return (
     <div>
       <h1 className="text-4xl font-bold text-center">Welcome to platform!</h1>
+      
       <Home serverSideParam={serverSideParam}/>
     </div>
   );
