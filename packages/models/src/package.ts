d5a6a0f90ef68158dbd9ae4dbb4ffaa119c2ffa3
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-01-19 23:17:35 +0100

    feat(models): added attributes for packages

--- packages/models/src/package.ts
+++ packages/models/src/package.ts
@@ -1,8 +1,25 @@
 import { z } from 'zod'
-import { LanguageSchema } from './language'
+
+export const PackageDurationSchema = z.object({
+    hours: z.number(),
+    minutes: z.number(),
+})
+export type TPackageDuration = z.infer<typeof PackageDurationSchema>
+
+export const PackagePricingSchema = z.object({
+    fullPrice: z.number(),
+    partialPrice: z.number(),
+    currency: z.string(),
+})
+export type TPackagePricing = z.infer<typeof PackagePricingSchema>
+
 
 export const PackageSchema = z.object({
-
+    title: z.string(),
+    imageUrl: z.string(),
+    duration: PackageDurationSchema,
+    description: z.string(),
+    pricing: PackagePricingSchema,
 })
 
 /**
