0ffc9d46a5c23f0d166e8c47bff531319a86be3e
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-02-02 15:34:53 +0100

    feat(models): implementation of the category model, with tests

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -2,3 +2,4 @@ export * as course from './course';
 export * as language from './language';
 export * as profile from './profile';
 export * as eClassPackage from './eclass-package';
+export * as category from './category';
