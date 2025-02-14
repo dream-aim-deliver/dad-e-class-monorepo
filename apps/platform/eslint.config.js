624d270cf20bac605a0bbed67d0fff6b4411e8fa
maany <imptodefeat@gmail.com>
2025-02-14 12:09:08 +0100

    chore(setup): backup paltform settings for eslint

--- apps/platform/eslint.config.js
+++ apps/platform/eslint.config.js
@@ -1,21 +1,25 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const js = require('@eslint/js');
-const { fixupConfigRules } = require('@eslint/compat');
-const nx = require('@nx/eslint-plugin');
-// const baseConfig = require('../../eslint.config.mjs/index.js');
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { fixupConfigRules } from '@eslint/compat';
+import nx from '@nx/eslint-plugin';
+import baseConfig from '../../eslint.config.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
 });
 
-module.exports = [
-  // ...fixupConfigRules(compat.extends('next')),
+export default [
+  ...fixupConfigRules(compat.extends('next')),
 
-  // ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
 
-  // ...baseConfig,
-  // ...nx.configs['flat/react-typescript'],
+  ...baseConfig,
+  ...nx.configs['flat/react-typescript'],
   {
     ignores: ['.next/**/*'],
   },
