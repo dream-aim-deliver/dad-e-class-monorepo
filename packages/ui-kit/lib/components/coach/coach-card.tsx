a47d2894941fc063cc8f1bebac00948a51a5205e
jatinrise <jatin@rise11.com>
2025-03-06 16:04:08 +0530

    feat(ui-kit): update CoachCard component to improve IconLanguage and IconCoachingSession styling

--- packages/ui-kit/lib/components/coach/coach-card.tsx
+++ packages/ui-kit/lib/components/coach/coach-card.tsx
@@ -105,11 +105,11 @@ const CoachCard: FC<CoachCardProps> = ({
         {/* Language & Session Count */}
         <div className="flex items-start gap-y-2 gap-x-4 w-full flex-wrap">
           <p className="flex items-center gap-1 lg:truncate">
-            <IconLanguage size="4" />
+            <IconLanguage classNames='flex-shrink-0' size="4" />
             <span className="capitalize truncate">{cardDetails.languages.join(', ')}</span>
           </p>
           <p className="flex items-center gap-1 truncate">
-            <IconCoachingSession size="4" />
+            <IconCoachingSession classNames='flex-shrink-0' size="4" />
             <span className="capitalize truncate">
               {cardDetails.sessionCount} {dictionary.components.coachCard.coachingSession}
               {cardDetails.sessionCount > 1 ? 's' : ''}
