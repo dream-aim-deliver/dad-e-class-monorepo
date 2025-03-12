0aa27a4a4f3ae9f28a1f89b93dcd9ee0efd2b189
divyanshu <divyanshug7897@gmail.com>
2025-03-12 13:07:08 +0000

    feat(ui-kit): enhance UserAvatar component to support conditional rendering of profile picture and initials

--- packages/ui-kit/lib/components/avatar/user-avatar.tsx
+++ packages/ui-kit/lib/components/avatar/user-avatar.tsx
@@ -1,27 +1,33 @@
 import { FC } from 'react';
 import { cn } from '../../utils/style-utils';
 
-export interface UserAvatarProps {
-  size?: 'xSmall' | 'small' | 'medium' | 'large' | 'xLarge'; // Defines avatar sizes
-  hasProfilePicture?: boolean; // Indicates if the user has a profile picture
-  imageUrl?: string; // URL of the profile picture
-  initials?: string; // Initials to display if no profile picture is present
-  className?: string; // Additional styling classes
+export interface UserAvatarWithPicture {
+  size?: 'xSmall' | 'small' | 'medium' | 'large' | 'xLarge';
+  hasProfilePicture: true;
+  imageUrl: string;
+  className?: string;
 }
-
-export const UserAvatar: FC<UserAvatarProps> = ({
-  size = 'medium',
-  hasProfilePicture=false,
-  initials = '',
-  imageUrl = '',
-  className,
-}) => {
-  /**
-   * Defines the size classes for different avatar sizes.
-   */
+export interface UserAvatarWithoutPicture {
+  size?: 'xSmall' | 'small' | 'medium' | 'large' | 'xLarge';
+  hasProfilePicture: false;
+  initials: string;
+  className?: string;
+}
+export type UserAvatarProps = UserAvatarWithPicture | UserAvatarWithoutPicture;
+/**
+ * A UserAvatar component that displays a user's profile picture or initials.
+ * @param size The size of the avatar ('xSmall', 'small', 'medium', 'large', 'xLarge'). Default is 'medium'.
+ * @param hasProfilePicture Boolean flag to determine if the user has a profile picture.
+ * @param imageUrl The URL of the user's profile picture (if available).
+ * @param initials The user's initials (used when there is no profile picture). Default is 'JF'.
+ * @param className Additional custom class names for styling.
+ * @returns A circular avatar component displaying either an image or initials.
+ */
+export const UserAvatar: FC<UserAvatarProps> = (props) => {
+  const { size = 'medium', className } = props;
   const sizeClasses = {
-    xSmall: 'w-6 h-6 text-xs',
-    small: 'w-6 h-6 text-sm',
+    xSmall: 'w-6 h-6 text-2xs',
+    small: 'w-8 h-8 text-sm',
     medium: 'w-12 h-12 text-sm',
     large: 'w-16 h-16 text-sm',
     xLarge: 'w-20 h-20 text-sm',
@@ -32,23 +38,20 @@ export const UserAvatar: FC<UserAvatarProps> = ({
       data-testid="user-avatar"
       className={cn(
         'flex items-center justify-center rounded-full',
-        !hasProfilePicture &&
-          'bg-base-neutral-700 text-text-secondary font-bold border border-base-neutral-600', // Styling when no profile picture
-        sizeClasses[size], // Apply size styles
+        !props.hasProfilePicture &&
+          'bg-base-neutral-700 text-text-secondary font-bold border border-base-neutral-600',
+        sizeClasses[size],
         className,
       )}
     >
-      {hasProfilePicture && imageUrl  ? (
-
-        // Render profile picture if available
+      {props.hasProfilePicture ? (
         <img
-          src={imageUrl}
+          src={props.imageUrl}
           alt="Profile"
           className={cn('w-full h-full object-cover rounded-full', className)}
         />
       ) : (
-        // Render initials if no profile picture
-        <span>{initials}</span>
+        'initials' in props && <span>{props.initials.slice(0, 2)}</span>
       )}
     </div>
   );
