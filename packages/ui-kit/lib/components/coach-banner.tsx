9f8e15ff7b1f88c99e6fcddf9d454866a38fa27b
divyanshu <divyanshug7897@gmail.com>
2025-03-13 10:33:14 +0000

    feat(ui-kit): add onClick prop and remove hardcoded ":"  to CoachBanner component for enhanced interactivity.

--- packages/ui-kit/lib/components/coach-banner.tsx
+++ packages/ui-kit/lib/components/coach-banner.tsx
@@ -7,6 +7,7 @@ interface CoachBannerProps {
   subtitle: string;
   description: string;
   imageUrl: string;
+  onClick?: () => void;
 }
 
 /**
@@ -27,14 +28,20 @@ interface CoachBannerProps {
  *  description="Description text" 
  *  imageUrl="https://example.com/image.jpg" />
  */
-export const CoachBanner: React.FC<isLocalAware & CoachBannerProps> = ({ locale, title, subtitle, description, imageUrl }) => {
+export const CoachBanner: React.FC<isLocalAware & CoachBannerProps> = ({
+  locale,
+  title,
+  subtitle,
+  description,
+  imageUrl,
+  onClick }) => {
   const dictionary = getDictionary(locale);
   return (
     <div className="flex flex-col items-start self-stretch max-w-7xl mx-auto py-8 md:py-12 px-[40px] rounded-[16px] border border-solid border-card-stroke bg-card-fill">
       <div className="grid grid-cols-1 md:grid-cols-2 items-start w-full h-full sm:gap-3">
         <div className="order-2 md:order-1 flex flex-col justify-start gap-4">
           <h2 className="text-2xl font-bold leading-[150%] text-base-white">
-            {title}: <br className="hidden md:block" /> {subtitle}
+            {title} <br className="hidden md:block" /> {subtitle}
           </h2>
           <p className="text-gray-300 text-md md:text-base leading-[150%] mb-6">
             {description}
@@ -44,6 +51,7 @@ export const CoachBanner: React.FC<isLocalAware & CoachBannerProps> = ({ locale,
             size="big"
             text={dictionary.components.coachBanner.buttontext}
             className="self-start"
+            onClick={onClick}
           />
         </div>
 
