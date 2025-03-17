a204a48c3e40d322476ff47976249faa19b8a2ff
maany <imptodefeat@gmail.com>
2025-03-17 23:26:07 +0100

    feat(auth): integrate NextAuth for authentication and session management

--- apps/platform/src/app/[locale]/page.tsx
+++ apps/platform/src/app/[locale]/page.tsx
@@ -11,8 +11,6 @@ export default async function Index() {
   const locale = await getLocale();
   return (
     <div className="bg-card-color-fill">
-      <h1 className="text-3xl font-extrablack text-base-neutral-50 text-center">Welcome to platform!</h1>
-      
       <Home locale={locale as TLocale}/>
     </div>
   );
