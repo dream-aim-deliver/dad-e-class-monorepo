2bede0c73e8059570b911d57a636667ecb247a75
maany <imptodefeat@gmail.com>
2025-03-17 23:25:48 +0100

    feat(translations): add new text fields for sign-in and welcome messages

--- packages/translations/src/lib/pages/home/home-de.ts
+++ packages/translations/src/lib/pages/home/home-de.ts
@@ -4,4 +4,8 @@ export const Home_DE: TDictionary["home"] = {
     title: 'Hallo Welt',
     buttonText: 'Klick mich',
     badgeText: 'Badge-12',
+    signInButtonText: 'Anmelden',
+    notSignedInText: 'Nicht angemeldet',
+    welcomeText: 'Willkommen',
+    signOut: 'Abmelden',
 };
--- packages/translations/src/lib/pages/home/home-en.ts
+++ packages/translations/src/lib/pages/home/home-en.ts
@@ -4,4 +4,8 @@ export const Home_EN: TDictionary["home"] = {
     title: 'Welcome to the app',
     buttonText: 'Click me',
     badgeText: 'Badge',
+    signInButtonText: 'Sign in',
+    notSignedInText: 'Not signed in',
+    welcomeText: 'Welcome',
+    signOut: 'Sign out',
 };
