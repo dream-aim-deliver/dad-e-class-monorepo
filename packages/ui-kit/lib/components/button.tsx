f4ebdfe6ecace57e855e0d0897b2687fc12bb495
jatinrise <jatin@rise11.com>
2025-03-17 18:51:07 +0530

    fix(button): update note for text truncation behavior in button component

--- packages/ui-kit/lib/components/button.tsx
+++ packages/ui-kit/lib/components/button.tsx
@@ -74,7 +74,7 @@ export interface ButtonProps extends VariantProps<typeof buttonStyles> {
  * @param hasIconRight Optional flag indicating whether an icon should be displayed on the right side of the button.
  * @param iconLeft Optional ReactNode representing an icon to display on the left side of the button. Only rendered if `hasIconLeft` is true.
  * @param iconRight Optional ReactNode representing an icon to display on the right side of the button. Only rendered if `hasIconRight` is true.
- * @Note if we find the maximum width of my button it will truncate my large text
+ * @Note if we pass the maximum width of the button in the className @prop, the text will be truncated automatically without affecting the size of icon.
  *
  * @example
  * <Button
