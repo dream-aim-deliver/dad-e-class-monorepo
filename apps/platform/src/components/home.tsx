62c17d99ddb1f443b99b70a8b7f517733fd07a9c
maany <imptodefeat@gmail.com>
2025-03-18 20:21:51 +0100

    feat(home): display session information and improve sign-in flow

--- apps/platform/src/components/home.tsx
+++ apps/platform/src/components/home.tsx
@@ -4,35 +4,40 @@ import '@maany_shr/e-class-ui-kit/tailwind.css';
 import { useTheme } from '@maany_shr/e-class-ui-kit/contexts';
 import { useTranslations } from 'next-intl';
 import { isLocalAware, TLocale } from '@maany_shr/e-class-translations';
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export type HomeProps = isLocalAware;
 
 export default function Home(props: HomeProps) {
   const { theme, setTheme } = useTheme();
   const t = useTranslations('home');
+  const { data: session } = useSession();
+  console.log('Session: ', session);
 
-  const { data: session } = useSession()
-  if(!session) {
+  if (!session) {
     return (
-      <div className='cursor-pointer text-center text-white'>
-        {t('notSignedInText')} <br/>
-        <button 
+      <div className="cursor-pointer text-center text-white">
+        {t('notSignedInText')} <br />
+        <button
           className="mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          onClick={() => signIn("auth0", undefined, { prompt: 'none' })}
+          onClick={() => signIn('auth0', undefined, { prompt: 'none' })}
         >
           {t('signInButtonText')}
         </button>
+        <div className="mt-4 text-sm text-gray-400">{session && session}</div>
       </div>
-    )
+    );
   }
- 
+  console.log('Session: ', session);
+
   return (
     <div className="flex flex-col  text-base-neutral-50 gap-4 mt-3 items-center justify-center text-center">
-      <p className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">{t('title')}</p>
-      <div className='text-center text-white text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl'>
+      <p className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+        {t('title')}
+      </p>
+      <div className="text-center text-white text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
         {t('welcomeText')} {session.user?.id} <br />
-        <button 
+        <button
           className="mt-4 px-6 py-2 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75"
           onClick={() => signOut()}
         >
@@ -52,7 +57,9 @@ export default function Home(props: HomeProps) {
         locale={props.locale as TLocale}
         skills={['React', 'TypeScript', 'TailwindCSS']}
       />
-      
+      <div className="flex flex-col text-wrap overflow-auto m-4 gap-2 p-4  text-sm text-gray-400">
+        {JSON.stringify(session, null, 4)}
+      </div>
     </div>
   );
 }
