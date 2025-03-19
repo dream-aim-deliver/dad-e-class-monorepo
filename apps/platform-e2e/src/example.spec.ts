3bd9fcf83ca80a6f276a3ab057d0fd429c5f11a1
maany <imptodefeat@gmail.com>
2025-03-19 19:24:07 +0100

    feat(platform-e2e): add an example test for logging in and seeing the welcome message

--- apps/platform-e2e/src/example.spec.ts
+++ apps/platform-e2e/src/example.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
-
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+test('has Welcome Message', async ({ page }) => {
+  await page.goto('http://localhost:3000/en/auth/login');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('Conny');
+  await page.getByText('Password').click();
+  await page.getByLabel('Password').fill('test');
+  await page.getByRole('button', { name: 'Sign In', exact: true }).click();
+  await expect(page.getByText('Welcome Conny')).toBeTruthy();
+  await expect(page.getByText('Welcome Conny')).toContainText('Welcome Conny');
 });
