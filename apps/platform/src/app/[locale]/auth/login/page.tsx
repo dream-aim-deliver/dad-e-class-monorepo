91145822adbc2e0434a1f577d357d793dc47e373
maany <imptodefeat@gmail.com>
2025-03-20 09:52:49 +0100

    fix(auth): handle optional chaining for user tokens in NextAuthGateway

--- apps/platform/src/app/[locale]/auth/login/page.tsx
+++ apps/platform/src/app/[locale]/auth/login/page.tsx
@@ -1,5 +1,5 @@
 import { TLocale } from "@maany_shr/e-class-translations";
-import LoginPageWithSuspense from "apps/platform/src/components/login";
+import LoginPageWithSuspense from "../../../../components/login";
 import { getLocale } from "next-intl/server";
 export default async function Login() {
   const platform = process.env.E_CLASS_PLATFORM_NAME || 'E-Class';
