564415afcf5a8c5586751bc5ec0282d1e0f0ba22
maany <imptodefeat@gmail.com>
2025-03-21 02:06:49 +0100

    feat(auth): add production check and handle Auth0 sign-in in login component

--- apps/platform/src/app/[locale]/auth/login/page.tsx
+++ apps/platform/src/app/[locale]/auth/login/page.tsx
@@ -1,11 +1,16 @@
 import { TLocale } from "@maany_shr/e-class-translations";
 import LoginPageWithSuspense from "../../../../components/login";
 import { getLocale } from "next-intl/server";
+import nextAuth  from "../../../../auth/config";
 export default async function Login() {
+  const signIn = nextAuth.signIn;
   const platform = process.env.E_CLASS_PLATFORM_NAME || 'E-Class';
+  const isProduction = process.env.NODE_ENV === 'production';
+  const isDevMode = process.env.E_CLASS_DEV_MODE?.trim().toLocaleLowerCase() === 'true';
   const enableTestAccounts = process.env.AUTH_ENABLE_TEST_ACCOUNTS?.trim().toLowerCase() === 'true';
+  const prod = isProduction && !isDevMode;
   const locale = await getLocale();
   return (
-    <LoginPageWithSuspense platform={platform} locale={locale as TLocale} enableCredentials={enableTestAccounts}/>
+    <LoginPageWithSuspense platform={platform} locale={locale as TLocale} enableCredentials={enableTestAccounts} isProduction={prod}/>
   )
 }
