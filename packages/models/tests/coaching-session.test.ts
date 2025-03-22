74c6c980c3b7cd42236c10e6a6aa2acace108db4
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-03-22 02:36:28 +0100

    fix(models): remove rating field from CoachingSession -- it's supposed to go in CoachingSessionReview instead

--- packages/models/tests/coaching-session.test.ts
+++ packages/models/tests/coaching-session.test.ts
@@ -7,7 +7,6 @@ describe('coaching session', () => {
         const validCoachingSession: TCoachingSession = {
             startTime: '2021-09-30T12:00:00.000Z',
             endTime: '2021-09-30T13:00:00.000Z',
-            rating: 4,
         }
         expect(CoachingSessionSchema.safeParse(validCoachingSession).success).toBe(true)
     });
@@ -15,17 +14,15 @@ describe('coaching session', () => {
     it('should invalidate a coaching session with missing required fields', () => {
         const invalidCoachingSession = {
             startTime: '2021-09-30T12:00:00.000Z',
-            endTime: '2021-09-30T13:00:00.000Z',
-            // Missing 'rating'
+            // Missing 'endTime'
         }
         expect(CoachingSessionSchema.safeParse(invalidCoachingSession).success).toBe(false)
     });
 
-    it('should invalidate a coaching session with an invalid rating', () => {
+    it('should invalidate a coaching session with an invalid time', () => {
         const invalidCoachingSession = {
-            startTime: '2021-09-30T12:00:00.000Z',
+            startTime: 'This time is invalid',
             endTime: '2021-09-30T13:00:00.000Z',
-            rating: 6,  // Rating should be between 0 and 5
         }
         expect(CoachingSessionSchema.safeParse(invalidCoachingSession).success).toBe(false)
     });
