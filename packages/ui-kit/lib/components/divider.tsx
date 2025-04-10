a4780d0723f97b856d18e13e2e57907d384be09f
MytsV <mytsvictoria@gmail.com>
2025-04-10 13:11:41 +0300

    fix(divider): adjust color and opacity

--- packages/ui-kit/lib/components/divider.tsx
+++ packages/ui-kit/lib/components/divider.tsx
@@ -1,5 +1,5 @@
 export function Divider({ className = "" }) {
   return (
-    <div className={`w-full border-t border-button-secondary-stroke my-12 ${className}`} />
+    <div className={`w-full border-t border-brand-divider/20 my-12 ${className}`} />
   );
 }
