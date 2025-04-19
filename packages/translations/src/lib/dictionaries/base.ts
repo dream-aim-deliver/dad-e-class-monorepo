b7596268fc9fb695112c50898628a9dca15d485a
divyanshu <divyanshug7897@gmail.com>
2025-04-19 11:53:24 +0000

    feat(review-dialog): add error handling and loading state to review submission process

--- packages/translations/src/lib/dictionaries/base.ts
+++ packages/translations/src/lib/dictionaries/base.ts
@@ -121,6 +121,7 @@ export const DictionarySchema = z.object({
       checkboxText: z.string(),
       yourReview: z.string(),
       thankYouText: z.string(),
+      errorState: z.string(),
     }),
     courseCard: z.object({
       createdBy: z.string(),
