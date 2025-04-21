94de022c4426e1d69ae81bed598c650610c3ef8b
Carla Bordo <carlapbg@gmail.com>
2025-04-21 22:29:51 +0200

    [TSK-821] - Style the Auth error page

--- apps/platform/src/app/[locale]/auth/error/page.tsx
+++ apps/platform/src/app/[locale]/auth/error/page.tsx
@@ -1,6 +1,7 @@
 import { TLocale } from "@maany_shr/e-class-translations";
 import ErrorPage from "../../../../components/login-error";
 import { getLocale } from "next-intl/server";
+
 export default async function Login() {
   const platform = process.env.E_CLASS_PLATFORM_SHORT_NAME || 'E-Class';
   const locale = await getLocale();
