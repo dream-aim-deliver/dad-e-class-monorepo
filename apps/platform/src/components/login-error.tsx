b98cf2adecacc49ba01b89e659baa7dabec73680
Carla Bordo <carlapbg@gmail.com>
2025-04-21 22:29:51 +0200

    [TSK-821] - Style the Auth error page

--- apps/platform/src/components/login-error.tsx
+++ apps/platform/src/components/login-error.tsx
@@ -2,6 +2,7 @@
 
 import { TLocale } from "@maany_shr/e-class-translations";
 import React from "react";
+import { Button } from '@maany_shr/e-class-ui-kit';
 
 interface ErrorPageProps {
     platform: string;
@@ -14,20 +15,20 @@ const ErrorPage = (props: ErrorPageProps) => {
   };
 
   return (
-    <div>
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="mb-8 text-4xl  font-bold"> SDA Planckster </h1>
-        <h5 className="10s mb-8 animate-pulse text-sm  font-bold">alpha</h5>
-        <div className="w-10"></div>
-        <div className="mt-8 space-y-4 rounded-md border border-gray-300 p-6 shadow-md">
-          <h2 className="text-2xl font-bold">Login</h2>
-          <h2>Something went wrong! Please check your credentials.</h2>
-          <button
-            className="mt-4 w-full rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+    <div className="theme-Cms flex items-center justify-center text-neutral-50">
+      <div className="flex flex-col items-center text-center space-y-6">
+        <h1 className="text-4xl font-bold">{props.platform}</h1>
+        <div className="space-y-4 rounded-md border border-base-neutral-700 bg-card-fill p-6 shadow-md">
+          <h2>Oops! There was a problem signing you in.</h2>
+          <p className="text-lg text-neutral-300">Please check your username and password and try again.</p>
+          <Button
+            variant="primary"
+            size="medium"
+            text={('Try Again')}
             onClick={tryAgain}
+            className="mx-auto"
           >
-            Try Again
-          </button>
+          </Button>
         </div>
       </div>
     </div>
