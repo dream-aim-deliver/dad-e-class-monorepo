a7b068931eb76bd09fa21b6c97c71b72e942db21
manishRise11 <anil@itrials.ai>
2025-05-06 10:13:04 +0530

    feat(ui-kit): Moving Milestone storybook to CourseOutline.

--- packages/ui-kit/stories/course-outline/milestone.stories.tsx
+++ packages/ui-kit/stories/course-outline/milestone.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Milestone } from '../../lib/components/course-outline/milestone';
 
 const meta: Meta<typeof Milestone> = {
-  title: 'Components/Milestone',
+  title: 'Components/CourseOutline/Milestone',
   component: Milestone,
   tags: ['autodocs'],
   parameters: {
