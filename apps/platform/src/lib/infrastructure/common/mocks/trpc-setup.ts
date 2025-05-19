ae2954f4a336e04c1448971ced9bceb6eb625c1f
MytsV <mytsvictoria@gmail.com>
2025-05-19 15:08:35 +0300

    feat(platform): align platform schema and integrate it

---
+++ apps/platform/src/lib/infrastructure/common/mocks/trpc-setup.ts
@@ -0,0 +1,7 @@
+import { initTRPC } from '@trpc/server';
+
+export const t = initTRPC.create();
+
+export const createTRPCRouter = t.router;
+export const createCallerFactory = t.createCallerFactory;
+export const baseProcedure = t.procedure;
