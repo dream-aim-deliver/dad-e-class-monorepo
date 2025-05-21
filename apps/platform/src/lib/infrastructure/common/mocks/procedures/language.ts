0b74c59d0f2351062d7b97869256a4d2cc7d0def
MytsV <mytsvictoria@gmail.com>
2025-05-21 12:35:18 +0300

    feat(platform): expect platform language ID from the view model

--- apps/platform/src/lib/infrastructure/common/mocks/procedures/language.ts
+++ apps/platform/src/lib/infrastructure/common/mocks/procedures/language.ts
@@ -2,14 +2,16 @@ import { language } from '@maany_shr/e-class-models';
 import { t } from '../trpc-setup';
 import { z } from 'zod';
 
-const listLanguagesMock: language.TLanguage[] = [
+const listLanguagesMock: language.TPlatformLanguage[] = [
     {
         code: 'en',
         name: 'English',
+        platformLanguageId: 1,
     },
     {
         code: 'de',
         name: 'Deutsch',
+        platformLanguageId: 2,
     },
 ];
 
@@ -19,7 +21,7 @@ const ListLanguageInputSchema = z.object({
 
 export const listLanguages = t.procedure
     .input(ListLanguageInputSchema)
-    .query(async (opts): Promise<language.TLanguage[]> => {
+    .query(async (opts): Promise<language.TPlatformLanguage[]> => {
         await new Promise((resolve) => setTimeout(resolve, 1000));
         return listLanguagesMock;
     });
--- packages/models/src/language.ts
+++ packages/models/src/language.ts
@@ -3,7 +3,12 @@ import { z } from "zod";
 
 export const LanguageSchema = z.object({
     name: z.string(),
-    code: z.string()
+    code: z.string(),
+});
+
+export const PlatformLanguageSchema = LanguageSchema.extend({
+    platformLanguageId: z.string().or(z.number()),
 });
 
 export type TLanguage = z.infer<typeof LanguageSchema>;
+export type TPlatformLanguage = z.infer<typeof PlatformLanguageSchema>;
