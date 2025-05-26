ec5689eba68a1ed2cf0bbf4f8b902352d4201399
Carla Bordo <carlapbg@gmail.com>
2025-05-27 00:28:16 +0200

    feat(ui-kit): add GroupCourseBanner component with translations, stories and tests. - Refactor user-avatar

---
+++ packages/ui-kit/lib/components/group-course-banner.tsx
@@ -0,0 +1,44 @@
+import { getDictionary, isLocalAware } from "@maany_shr/e-class-translations";
+import { Button } from "./button";
+
+export interface GroupCourseBannerProps extends isLocalAware {
+  studentsName: string;
+  avatarImageUrl: string;
+  children: React.ReactNode;
+  onClickGroupWorkspace: () => void;
+}
+
+export const GroupCourseBanner = ({
+  studentsName,
+  avatarImageUrl,
+  onClickGroupWorkspace,
+  children,
+  locale,
+}: GroupCourseBannerProps) => {
+    const dictionary = getDictionary (locale);
+    
+    return (
+    <div className="flex flex-row w-full p-3 items-center gap-16 bg-base-neutral-800 rounded-small border border-base-neutral-700">
+        <div className="flex flex-col gap-3 justify-start items-left">
+            <p className="text-sm text-text-primary font-important"> {dictionary.components.groupCourseBanner.takenAlsoBy} </p>
+             <div className="flex flex-row items-center gap-4"> 
+                {children}                
+                <p className="text-sm text-text-primary">
+                  {studentsName}
+                </p>
+            </div>
+        </div>
+        <div className="flex-1 flex justify-end">
+            <Button
+                size="medium"
+                variant="primary"
+                text={dictionary.components.groupCourseBanner.groupWorkspaceButton}
+                onClick={onClickGroupWorkspace}
+            />
+          </div>
+
+    </div>
+
+)
+};
+
