ef9d2dbfb46ef2e66fae434427141ff3572fad20
MytsV <mytsvictoria@gmail.com>
2025-06-05 11:53:17 +0300

    fix(ui-kit, base-grid): match skeleton with the AG Grid

---
+++ packages/ui-kit/lib/components/skeletons/grid-skeleton.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from './skeleton';
+
+export default function GridSkeleton() {
+  return <Skeleton className="absolute flex items-center justify-center w-full h-full rounded-b-none animate-none z-10 rounded-[8px]"/>
+}
