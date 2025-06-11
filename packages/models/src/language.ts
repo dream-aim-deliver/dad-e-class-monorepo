59c0c6ddb7c93e25f24515086880d34cd8d56064
MytsV <mytsvictoria@gmail.com>
2025-06-11 15:27:28 +0300

    fix(models): return platform language id to the models

--- packages/models/src/language.ts
+++ packages/models/src/language.ts
@@ -1,9 +1,14 @@
 import { z } from "zod";
 
-
 export const LanguageSchema = z.object({
     name: z.string(),
     code: z.string()
 });
 
+export const PlatformLanguageSchema = LanguageSchema.extend({
+    platformLanguageId: z.string().or(z.number()),
+});
+
 export type TLanguage = z.infer<typeof LanguageSchema>;
+export type TPlatformLanguage = z.infer<typeof PlatformLanguageSchema>;
+export type TPlatformLanguageId = TPlatformLanguage["platformLanguageId"];
