76df76a29f965bf696926f3a7b034f58dd16571d
MytsV <mytsvictoria@gmail.com>
2025-06-11 14:09:21 +0300

    feat(platform): configure proper caching of home page view model

--- apps/platform/src/app/api/hello/route.ts
+++ apps/platform/src/app/api/hello/route.ts
@@ -1,3 +1,8 @@
+import { revalidateTag } from 'next/cache';
+import { cacheTags } from '../../../lib/infrastructure/server/utils/cache-tags';
+import { env } from '../../../lib/infrastructure/server/utils/env';
+
 export async function GET(request: Request) {
+    revalidateTag(`${cacheTags.HOME_PAGE}-${env.platformId}`);
     return new Response('Hello, from API!!');
 }
