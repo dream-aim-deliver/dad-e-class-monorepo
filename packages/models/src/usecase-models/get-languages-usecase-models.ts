6bbaab96b6eba8e6f16b5fe7583e122da3ffde02
MytsV <mytsvictoria@gmail.com>
2025-06-13 18:57:16 +0300

    feat(models, platform, layout): integrate languages

---
+++ packages/models/src/usecase-models/get-languages-usecase-models.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { BaseErrorDiscriminatedUnionSchemaFactory, BaseStatusDiscriminatedUnionSchemaFactory, BaseSuccessSchemaFactory } from '../cats/cats-core';
+
+export const GetLanguagesRequestSchema = z.object({});
+export type TGetLanguagesRequest = z.infer<typeof GetLanguagesRequestSchema>;
+
+const GetLanguagesSuccessResponseSchema = BaseSuccessSchemaFactory(z.object({
+    languages: z.array(z.object({
+        languageCode: z.string(),
+        language: z.string(),
+    }))
+}));
+
+export type TGetLanguagesSuccessResponse = z.infer<typeof GetLanguagesSuccessResponseSchema>;
+
+const GetLanguagesUseCaseErrorResponseSchema = BaseErrorDiscriminatedUnionSchemaFactory({});
+export type TGetLanguagesUseCaseErrorResponse = z.infer<typeof GetLanguagesUseCaseErrorResponseSchema>;
+export const GetLanguagesUseCaseResponseSchema = BaseStatusDiscriminatedUnionSchemaFactory([
+    GetLanguagesSuccessResponseSchema,
+    GetLanguagesUseCaseErrorResponseSchema,
+]);
+export type TGetLanguagesUseCaseResponse = z.infer<typeof GetLanguagesUseCaseResponseSchema>;
