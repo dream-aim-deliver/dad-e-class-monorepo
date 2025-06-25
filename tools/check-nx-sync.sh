10e70baa7cb7d99af3bc946867e188e1090ca4bb
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-06-25 19:34:11 +0200

    fix(script): improve error logging in check-nx-sync.sh for debugging

--- tools/check-nx-sync.sh
+++ tools/check-nx-sync.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-pnpm nx sync &> /dev/null
+pnpm nx sync &> /tmp/nxsync.log
 
-git diff --exit-code &> /dev/null
+git diff --exit-code &> /tmp/nxsync.diff
 
 if [ $? -ne 0 ]; then
   echo "NxSyncError: The workspace is out of sync. Please run 'pnpm nx sync', commit your changes, and try again." >&2
+  echo "--------------------------------------------------" >&2
+  echo "Nx log:" >&2
+  cat /tmp/nxsync.log >&2
+  echo "--------------------------------------------------" >&2
+  echo "See the diff below for details:" >&2
+  cat /tmp/nxsync.diff >&2
   exit 1
 fi
 
+rm /tmp/nxsync.log
+rm /tmp/nxsync.diff
