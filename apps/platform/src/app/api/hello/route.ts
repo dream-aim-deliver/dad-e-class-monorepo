f8c82a5f1fd74fcbdb1d2717bc97219ff1a93fa3
MytsV <mytsvictoria@gmail.com>
2025-06-25 14:14:38 +0300

    fix(platform): remove unused route

--- apps/platform/src/app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-import { revalidateTag } from 'next/cache';
-import { cacheTags } from '../../../lib/infrastructure/server/utils/cache-tags';
-import { env } from '../../../lib/infrastructure/server/utils/env';
-
-export async function GET(request: Request) {
-    revalidateTag(`${cacheTags.HOME_PAGE}-${env.platformId}`);
-    return new Response('Hello, from API!!');
-}
