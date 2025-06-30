3419812fafa19f6c0f9481d932e506f58dabf13f
maany <imptodefeat@gmail.com>
2025-06-30 21:39:13 +0200

    refactor(ui-kit): improve formatting and readability in HomeAccordion component

--- packages/ui-kit/lib/components/home-accordion.tsx
+++ packages/ui-kit/lib/components/home-accordion.tsx
@@ -14,35 +14,67 @@ export interface HomeAccordionProps {
     showNumbers?: boolean;
 }
 
-export function HomeAccordion({ title, items, showNumbers }: HomeAccordionProps) {
+export function HomeAccordion({
+    title,
+    items,
+    showNumbers,
+}: HomeAccordionProps) {
     const defaultValue = items?.length ? [items[0].title] : [];
 
     return (
-        <Accordion className="flex flex-col gap-7" type="single" defaultValue={defaultValue}>
+        <Accordion
+            className="flex flex-col gap-7"
+            type="single"
+            defaultValue={defaultValue}
+        >
             <h3 className="text-text-primary lg:text-mega text-2xl">{title}</h3>
             <div className="w-full">
                 {items?.map((item, index) => (
                     <AccordionItem
                         className={cn(
                             'py-6',
-                            items.length - 1 !== index && 'border-b border-divider'
+                            items.length - 1 !== index &&
+                                'border-b border-divider',
                         )}
                         key={item.title}
                         value={item.title}
                     >
                         <AccordionTrigger value={item.title}>
                             <div className="flex items-center gap-4">
-                                {showNumbers && item.position &&
-                                    <h4 className="text-action-default">{item.position}.</h4>}
+                                {showNumbers && item.position && (
+                                    <h4 className="text-action-default">
+                                        {item.position}.
+                                    </h4>
+                                )}
                                 <div className="flex items-center gap-4">
-                                    {item.iconImageUrl && <img alt={item.title} src={item.iconImageUrl} className="w-8 h-8"/>}
-                                    <h5 className="text-text-primary font-medium">{item.title}</h5>
+                                    {item.iconImageUrl && (
+                                        <img
+                                            alt={item.title}
+                                            src={item.iconImageUrl}
+                                            className="w-8 h-8"
+                                        />
+                                    )}
+                                    <h5 className="text-text-primary font-medium">
+                                        {item.title}
+                                    </h5>
                                 </div>
                             </div>
                         </AccordionTrigger>
-                        <AccordionContent value={item.title} className={cn("pt-4", showNumbers && "pl-8")}>
-                            <RichTextRenderer content={item.content}
-                                              className="lg:text-md text-normal leading-[150%]  text-text-secondary" />
+                        <AccordionContent
+                            value={item.title}
+                            className={cn('pt-4', showNumbers && 'pl-8')}
+                        >
+                            <RichTextRenderer
+                                content={item.content}
+                                className="lg:text-md text-normal leading-[150%]  text-text-secondary"
+                                onDeserializationError={(message, error) => {
+                                    console.error(
+                                        'Error deserializing content:',
+                                        message,
+                                        error,
+                                    );
+                                }}
+                            />
                         </AccordionContent>
                     </AccordionItem>
                 ))}
