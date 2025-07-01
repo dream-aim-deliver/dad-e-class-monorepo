f4975ce7f82f74f701d3b8b4f456bc1cbe56d7f6
MytsV <mytsvictoria@gmail.com>
2025-07-01 17:36:19 +0300

    fix(platform): pass locale in default loading wrapper

--- apps/platform/src/lib/infrastructure/client/wrappers/default-loading.tsx
+++ apps/platform/src/lib/infrastructure/client/wrappers/default-loading.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { TLocale } from '@maany_shr/e-class-translations';
 import { DefaultLoading } from '@maany_shr/e-class-ui-kit';
+import { useLocale } from 'next-intl';
 
 export default function DefaultLoadingWrapper() {
-    return <DefaultLoading />;
+    const locale = useLocale() as TLocale
+    return <DefaultLoading locale={locale} />;
 }
