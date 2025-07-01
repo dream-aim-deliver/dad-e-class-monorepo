58979b5dd11634e43ba683535c1f757a29c88356
MytsV <mytsvictoria@gmail.com>
2025-07-01 12:39:39 +0300

    feat(ui-kit): implement proper interface for default error

--- packages/ui-kit/lib/components/default-error.tsx
+++ packages/ui-kit/lib/components/default-error.tsx
@@ -1,7 +1,24 @@
+import { TLocale, getDictionary } from "@maany_shr/e-class-translations";
+import Banner from "./banner";
+
 interface DefaultErrorProps {
-    errorMessage?: string;
+    locale: TLocale;
+    title?: string;
+    description?: string;
+    onRetry?: () => void;
 }
 
+// TODO: properly design the default error
 export default function DefaultError(props: DefaultErrorProps) {
-    return <span className="text-neutral-100">{props.errorMessage ?? "An unknown error happened"}</span>;
+    const dictionary = getDictionary(props.locale);
+
+    return <Banner 
+        title={props.title || dictionary.components.defaultError.title}
+        description={props.title || dictionary.components.defaultError.description}
+        style="error"
+        button={props.onRetry && {
+            onClick: props.onRetry,
+            label: dictionary.components.defaultError.retry,
+        }}
+    />;
 }
