d2af59223cd4ecc7a555341ccb65536474d30560
MytsV <mytsvictoria@gmail.com>
2025-07-01 14:13:09 +0300

    feat(platform): refactor error handling for offers

--- packages/models/src/view-models/coaching-page-view-model.ts
+++ packages/models/src/view-models/coaching-page-view-model.ts
@@ -12,12 +12,10 @@ export type TCoachingPageSuccess = z.infer<typeof CoachingPageSuccessSchema>;
 
 const CoachingPageDefaultViewModelSchema = BaseDiscriminatedViewModeSchemaFactory("default", CoachingPageSuccessSchema)
 const CoachingPageKaboomViewModelSchema = BaseDiscriminatedViewModeSchemaFactory("kaboom", BaseErrorDataSchemaFactory())
-const CoachingPageUnauthenticatedViewModelSchema = BaseDiscriminatedViewModeSchemaFactory("unauthenticated", BaseErrorDataSchemaFactory())
 
 export const CoachingPageViewModelSchemaMap = {
     default: CoachingPageDefaultViewModelSchema,
     kaboom: CoachingPageKaboomViewModelSchema,
-    unauthenticated: CoachingPageUnauthenticatedViewModelSchema,
 };
 export type TCoachingPageViewModelSchemaMap = typeof CoachingPageViewModelSchemaMap;
 export const CoachingPageViewModelSchema = BaseViewModelDiscriminatedUnionSchemaFactory(CoachingPageViewModelSchemaMap);
