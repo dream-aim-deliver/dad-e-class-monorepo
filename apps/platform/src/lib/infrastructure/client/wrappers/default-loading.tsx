5c87ee787eab96a329250b6418974d816eb8f804
MytsV <mytsvictoria@gmail.com>
2025-07-01 17:41:57 +0300

    feat(platform): ensure authentication for available coachings

--- apps/platform/src/lib/infrastructure/client/wrappers/default-loading.tsx
+++ apps/platform/src/lib/infrastructure/client/wrappers/default-loading.tsx
@@ -5,6 +5,6 @@ import { DefaultLoading } from '@maany_shr/e-class-ui-kit';
 import { useLocale } from 'next-intl';
 
 export default function DefaultLoadingWrapper() {
-    const locale = useLocale() as TLocale
+    const locale = useLocale() as TLocale;
     return <DefaultLoading locale={locale} />;
 }
