f123c35cc1bfc66fa94329c49e8383b75b0eefc9
MytsV <mytsvictoria@gmail.com>
2025-07-01 12:54:40 +0300

    feat(platform): refactor error handling in home page

--- apps/platform/src/lib/infrastructure/client/hooks/use-home-page-presenter.ts
+++ apps/platform/src/lib/infrastructure/client/hooks/use-home-page-presenter.ts
@@ -3,13 +3,11 @@ import HomePagePresenter, {
 } from '../../common/presenters/home-page-presenter';
 import { viewModels } from '@maany_shr/e-class-models';
 import { useMemo } from 'react';
-import { useDefaultPresenterUtilities } from '../utils/use-default-presenter-utilities';
 
 export function useGetHomePagePresenter(
     setViewModel: (viewModel: viewModels.THomePageViewModel) => void,
 ) {
-    const presenterUtilities: THomePagePresenterUtilities =
-        useDefaultPresenterUtilities();
+    const presenterUtilities: THomePagePresenterUtilities = {};
     const presenter = useMemo(
         () => new HomePagePresenter(setViewModel, presenterUtilities),
         [setViewModel],
--- apps/platform/src/lib/infrastructure/client/hooks/use-topics-presenter.ts
+++ apps/platform/src/lib/infrastructure/client/hooks/use-topics-presenter.ts
@@ -3,13 +3,11 @@ import { useMemo } from 'react';
 import TopicsPresenter, {
     TTopicsPresenterUtilities,
 } from '../../common/presenters/topics-presenter';
-import { useDefaultPresenterUtilities } from '../utils/use-default-presenter-utilities';
 
 export function useListTopicsPresenter(
     setViewModel: (viewModel: viewModels.TTopicListViewModel) => void,
 ) {
-    const presenterUtilities: TTopicsPresenterUtilities =
-        useDefaultPresenterUtilities();
+    const presenterUtilities: TTopicsPresenterUtilities = {};
     const presenter = useMemo(
         () => new TopicsPresenter(setViewModel, presenterUtilities),
         [setViewModel, presenterUtilities],
