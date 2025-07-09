d6c32a49bbd906b96a21b3ada76b0639d52ff9e9
MytsV <mytsvictoria@gmail.com>
2025-07-09 11:51:33 +0300

    fix(ui-kit): fix UI tests and stories for course intro banner

--- packages/ui-kit/stories/course-intro-banner.stories.tsx
+++ packages/ui-kit/stories/course-intro-banner.stories.tsx
@@ -27,7 +27,7 @@ type Story = StoryObj<typeof CourseIntroBanner>;
 
 export const Default: Story = {
   args: {
-    description: 'Discover the art of moving images in our practice-oriented course, led by industry professionals! You will learn how to develop an effective storyboard and a convincing concept. We offer you professional tips and support you in the implementation of your project. We will also show you the best tools to realize your ideas quickly and efficiently. Stand out from the crowd with a unique look and innovative ideas and win n.',
+    description: '[{"type": "paragraph", "children": [{"text": "Discover the art of moving images in our practice-oriented course, led by industry professionals! You will learn how to develop an effective storyboard and a convincing concept. We offer you professional tips and support you in the implementation of your project. We will also show you the best tools to realize your ideas quickly and efficiently. Stand out from the crowd with a unique look and innovative ideas and win n."}]}]',
     videoId: 'TvV02y7Wpo1use1agD7fbqPob0101WKBOjYe02j01jVrgubw',
     thumbnailUrl: 'https://res.cloudinary.com/dryynqhao/image/upload/v1742541099/lrpuzzgdayhoirs4gqgj.png',
     locale: 'en',
@@ -37,7 +37,7 @@ export const Default: Story = {
 
 export const LongTitle: Story = {
   args: {
-    description: 'Join thousands of learners in building scalable, efficient applications from scratch. Learn from industry experts and develop the skills you need to land a job as a software engineer. Start learning now!',
+    description: '[{"type": "paragraph", "children": [{"text": "Join thousands of learners in building scalable, efficient applications from scratch. Learn from industry experts and develop the skills you need to land a job as a software engineer. Start learning now!"}]}]',
     videoId: 'TvV02y7Wpo1use1agD7fbqPob0101WKBOjYe02j01jVrgubw',
     thumbnailUrl: 'https://res.cloudinary.com/dryynqhao/image/upload/v1742541099/lrpuzzgdayhoirs4gqgj.png',
     locale: 'de',
@@ -47,7 +47,7 @@ export const LongTitle: Story = {
 
 export const NoThumbnail: Story = {
   args: {
-    description: 'Discover the art of moving images in our practice-oriented course, led by industry professionals! You will learn how to develop an effective storyboard and a convincing concept. We offer you professional tips and support you in the implementation of your project. We will also show you the best tools to realize your ideas quickly and efficiently. Stand out from the crowd with a unique look and innovative ideas and win n.',
+    description: '[{"type": "paragraph", "children": [{"text": "Discover the art of moving images in our practice-oriented course, led by industry professionals! You will learn how to develop an effective storyboard and a convincing concept. We offer you professional tips and support you in the implementation of your project. We will also show you the best tools to realize your ideas quickly and efficiently. Stand out from the crowd with a unique look and innovative ideas and win n."}]}]',
     videoId: 'TvV02y7Wpo1use1agD7fbqPob0101WKBOjYe02j01jVrgubw',
     locale: 'en',
     onErrorCallback: (message, error) => console.log('error', message, error),
@@ -56,7 +56,7 @@ export const NoThumbnail: Story = {
 
 export const NoVideo: Story = {
   args: {
-    description: 'Discover the art of moving images in our practice-oriented course, led by industry professionals! You will learn how to develop an effective storyboard and a convincing concept. We offer you professional tips and support you in the implementation of your project. We will also show you the best tools to realize your ideas quickly and efficiently. Stand out from the crowd with a unique look and innovative ideas and win n.',
+    description: '[{"type": "paragraph", "children": [{"text": "Discover the art of moving images in our practice-oriented course, led by industry professionals! You will learn how to develop an effective storyboard and a convincing concept. We offer you professional tips and support you in the implementation of your project. We will also show you the best tools to realize your ideas quickly and efficiently. Stand out from the crowd with a unique look and innovative ideas and win n."}]}]',
     thumbnailUrl: 'https://res.cloudinary.com/dryynqhao/image/upload/v1742541099/lrpuzzgdayhoirs4gqgj.png',
     locale: 'en',
     onErrorCallback: (message, error) => console.log('error', message, error),
