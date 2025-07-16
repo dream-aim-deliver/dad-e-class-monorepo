a6f4422e6a29768a8b0f5717d62ed4e1d01476e9
MytsV <mytsvictoria@gmail.com>
2025-07-16 11:13:47 +0300

    Add coach availability card component and events

--- packages/ui-kit/stories/calendar-weekly.stories.tsx
+++ packages/ui-kit/stories/calendar-weekly.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { WeeklyCalendar } from '../lib/components/calendar/weekly-calendar';
+import { AvailabilityCalendarCard } from '../lib/components/calendar/calendar-cards';
 
 export default {
     title: 'Components/Calendar/CalendarWeekly',
@@ -17,5 +18,12 @@ export const Default: Story = {
         currentDate: new Date(),
         setCurrentDate: (date: Date) => console.log('Current date set to:', date),
         locale: 'en',
+        events: [
+            {
+                start: new Date('2025-07-16T10:00:00'),
+                end: new Date('2025-07-16T12:15:00'),
+                component: <AvailabilityCalendarCard locale="en" start={new Date('2025-07-16T10:00:00')} end={new Date('2025-07-16T12:15:00')} />,
+            }
+        ]
     },
 };
