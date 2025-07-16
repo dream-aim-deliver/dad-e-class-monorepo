5596020d84bab346a7036d964de248f143a3552b
MytsV <mytsvictoria@gmail.com>
2025-07-16 11:32:26 +0300

    Integrate all session cards and respective mocks

--- packages/ui-kit/lib/components/calendar/calendar-cards.tsx
+++ packages/ui-kit/lib/components/calendar/calendar-cards.tsx
@@ -18,14 +18,46 @@ export function AvailabilityCalendarCard(props: AvailabilityCalendarCardProps) {
 
     return (
         <div
-            className="h-full w-full rounded-md bg-action-semi-transparent-medium text-action-default font-semibold p-2 text-sm cursor-pointer"
+            className={`h-full w-full rounded-md bg-action-semi-transparent-medium text-action-default font-semibold p-2 text-sm overflow-hidden ${props.onClick ? 'cursor-pointer' : ''}`}
             onClick={props.onClick}
         >
-            {timeRange}
+            <div className="truncate" title={timeRange}>
+                {timeRange}
+            </div>
         </div>
     );
 }
 
-export function SessionCalendarCard() {
+interface SessionCalendarCardProps {
+    locale: string;
+    start: Date;
+    end: Date;
+    onClick?: () => void;
+    title: string;
+}
 
+export function SessionCalendarCard(props: SessionCalendarCardProps) {
+    const formatTime = (date: Date) => {
+        return date.toLocaleTimeString(props.locale, {
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: undefined,
+        });
+    };
+
+    const timeRange = `${formatTime(props.start)} - ${formatTime(props.end)}`;
+
+    return (
+        <div
+            className={`h-full w-full rounded-md bg-action-default text-text-primary-inverted font-semibold p-2 text-sm overflow-hidden ${props.onClick ? 'cursor-pointer' : ''}`}
+            onClick={props.onClick}
+        >
+            <div className="truncate" title={timeRange}>
+                {timeRange}
+            </div>
+            <div className="truncate" title={props.title}>
+                {props.title}
+            </div>
+        </div>
+    );
 }
