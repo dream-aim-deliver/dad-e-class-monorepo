1a9d021aba7513850d24c829e7c1115794dd1260
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-08-05 03:12:43 +0200

    chore(ui-kit): added alert callback to breadcrumbs story

--- packages/ui-kit/stories/breadcrumbs.stories.tsx
+++ packages/ui-kit/stories/breadcrumbs.stories.tsx
@@ -2,31 +2,40 @@ import { Meta, StoryObj } from '@storybook/react';
 import { Breadcrumbs } from '../lib/components/breadcrumbs';
 
 export default {
-    title: 'Components/Breadcrumbs',
-    component: Breadcrumbs,
-    tags: ['autodocs'],
-    parameters: {
-        layout: 'centered',
+  title: 'Components/Breadcrumbs',
+  component: Breadcrumbs,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    items: {
+      description: 'List of breadcrumb items with labels and click handlers',
     },
-    argTypes: {
-        items: {
-            description:
-                'List of breadcrumb items with labels and click handlers',
-        },
-    },
+  },
 } satisfies Meta<typeof Breadcrumbs>;
 
 type Story = StoryObj<typeof Breadcrumbs>;
 
+const onClick = () => {
+  alert(`Breadcrumb clicked`)
+}
+
 export const Default: Story = {
-    args: {
-        items: [
-            { label: 'Home', onClick: () => alert('Clicked on Home') },
-            {
-                label: 'Dashboard',
-                onClick: () => alert('Clicked on Dashboard'),
-            },
-            { label: 'Settings', onClick: () => alert('Clicked on Settings') },
-        ],
-    },
+  args: {
+    items: [
+      {
+        label: 'Home',
+        onClick: onClick,
+      },
+      {
+        label: 'Dashboard',
+        onClick: onClick,
+      },
+      {
+        label: 'Settings',
+        onClick: onClick,
+      },
+    ],
+  },
 };
