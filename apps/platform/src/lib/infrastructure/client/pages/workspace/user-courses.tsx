e8faa423a6f6d32a3e84508f0b8b2fb440634b58
MytsV <mytsvictoria@gmail.com>
2025-08-07 18:05:53 +0300

    Start integrating user course view

---
+++ apps/platform/src/lib/infrastructure/client/pages/workspace/user-courses.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import { useState } from 'react';
+import { trpc } from '../../trpc/client';
+import { viewModels } from '@maany_shr/e-class-models';
+import { useListUserCoursesPresenter } from '../../hooks/use-user-courses-presenter';
+import {
+    Breadcrumbs,
+    Button,
+    CardListLayout,
+    CoachCourseCard,
+    DefaultError,
+    DefaultLoading,
+    DefaultNotFound,
+    PageTitle,
+} from '@maany_shr/e-class-ui-kit';
+import { useLocale } from 'next-intl';
+import { TLocale } from '@maany_shr/e-class-translations';
+
+export default function UserCourses() {
+    const locale = useLocale() as TLocale;
+
+    const [coursesResponse] = trpc.listUserCourses.useSuspenseQuery({
+        pagination: {
+            page: 1,
+            pageSize: 6,
+        },
+    });
+    const [coursesViewModel, setCoursesViewModel] = useState<
+        viewModels.TUserCourseListViewModel | undefined
+    >(undefined);
+    const { presenter } = useListUserCoursesPresenter(setCoursesViewModel);
+    presenter.present(coursesResponse, coursesViewModel);
+
+    if (!coursesViewModel) {
+        return <DefaultLoading locale={locale} />;
+    }
+
+    if (coursesViewModel.mode === 'not-found') {
+        return <DefaultNotFound locale={locale} />;
+    }
+
+    if (coursesViewModel.mode === 'kaboom') {
+        return <DefaultError locale={locale} />;
+    }
+
+    const courses = coursesViewModel.data.courses;
+
+    if (courses.length === 0) {
+        return <DefaultNotFound locale={locale} />;
+    }
+
+    return (
+        <div className="flex flex-col space-y-2">
+            <Breadcrumbs
+                items={[
+                    { label: 'Home', onClick: () => {} },
+                    { label: 'Workspace', onClick: () => {} },
+                    { label: 'Courses', onClick: () => {} },
+                ]}
+            />
+            <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:justify-between sm:items-center">
+                <PageTitle text="Your Courses" />
+                <Button text="Create a course" />
+            </div>
+            <CardListLayout>
+                {courses.map((course) => {
+                    if (course.role === 'coach') {
+                        return (
+                            <CoachCourseCard
+                                key={course.id}
+                                reviewCount={course.reviewCount}
+                                sessions={course.coachingSessionCount ?? 0}
+                                sales={course.salesCount}
+                                locale={locale}
+                                language={{
+                                    code: '',
+                                    name: course.languages[0],
+                                }}
+                                title={''}
+                                imageUrl={course.imageUrl ?? ''}
+                                author={{
+                                    name:
+                                        course.author.name +
+                                        ' ' +
+                                        course.author.surname,
+                                    image: course.author.avatarUrl ?? '',
+                                }}
+                                duration={{
+                                    selfStudy: course.fullDuration,
+                                    video: 0,
+                                    coaching: 0,
+                                }}
+                                rating={course.averageRating}
+                            />
+                        );
+                    }
+                    return <div key={course.id}>Mock</div>;
+                })}
+            </CardListLayout>
+        </div>
+    );
+}
