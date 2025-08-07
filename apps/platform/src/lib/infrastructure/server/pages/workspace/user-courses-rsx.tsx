cd80845ae8ba93803cadb4d958d98b276162da4a
MytsV <mytsvictoria@gmail.com>
2025-08-07 14:41:01 +0300

    Cache session DTO inside a single request

--- apps/platform/src/lib/infrastructure/server/pages/workspace/user-courses-rsx.tsx
+++ apps/platform/src/lib/infrastructure/server/pages/workspace/user-courses-rsx.tsx
@@ -1,23 +1,15 @@
 import { HydrateClient, prefetch, trpc } from '../../config/trpc/server';
 import { Suspense } from 'react';
 import DefaultLoadingWrapper from '../../../client/wrappers/default-loading';
-import { NextAuthGateway } from '@maany_shr/e-class-auth';
-import { auth } from '@maany_shr/e-class-models';
-import nextAuth from '../../config/auth/next-auth.config';
 import { redirect } from 'next/navigation';
+import getSession from '../../config/auth/get-session';
 
 interface UserCoursesProps {}
 
 export default async function UserCoursesServerComponent(
     props: UserCoursesProps,
 ) {
-    // TODO: Use React's cache() to not re-fetch the session on every request
-    const authGateway = new NextAuthGateway(nextAuth);
-    const sessionDTO = await authGateway.getSession();
-    let session: auth.TSession | null = null;
-    if (sessionDTO.success) {
-        session = sessionDTO.data;
-    }
+    const session = await getSession();
 
     if (!session || !session.user) {
         redirect('/auth/login');
