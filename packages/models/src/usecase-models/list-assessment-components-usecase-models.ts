d53516559c937f84c3b757a3e0aeefb708f2a45b
MytsV <mytsvictoria@gmail.com>
2025-08-11 16:51:23 +0300

    Add models for listing lesson components usecase

--- packages/models/src/usecase-models/list-assessment-components-usecase-models.ts
+++ packages/models/src/usecase-models/list-assessment-components-usecase-models.ts
@@ -4,6 +4,7 @@ import {
     BaseStatusDiscriminatedUnionSchemaFactory,
     BaseSuccessSchemaFactory
 } from '@dream-aim-deliver/dad-cats';
+import { AssessmentComponentSchema } from './common';
 
 export const ListAssessmentComponentsRequestSchema = z.object({
   courseSlug: z.string(),
@@ -11,68 +12,8 @@ export const ListAssessmentComponentsRequestSchema = z.object({
 
 export type TListAssessmentComponentsRequest = z.infer<typeof ListAssessmentComponentsRequestSchema>;
 
-const BaseComponent = z.object({
-  id: z.string(),
-  order: z.number(),
-  type: z.string(),
-});
-
-const OptionSchema = z.object({
-  id: z.number(),
-  name: z.string(),
-});
-
-const RichTextSchema = BaseComponent.extend({
-  type: z.literal('richText'),
-  text: z.string(),
-});
-
-const HeadingSchema = BaseComponent.extend({
-  type: z.literal('heading'),
-  text: z.string(),
-  size: z.string(),
-});
-
-const TextInputSchema = BaseComponent.extend({
-  type: z.literal('textInput'),
-  helperText: z.string(),
-  required: z.boolean(),
-});
-
-const SingleChoiceSchema = BaseComponent.extend({
-  type: z.literal('singleChoice'),
-  title: z.string(),
-  options: z.array(OptionSchema),
-  required: z.boolean(),
-});
-
-const MultipleChoiceSchema = BaseComponent.extend({
-  type: z.literal('multipleChoice'),
-  title: z.string(),
-  options: z.array(OptionSchema),
-  required: z.boolean(),
-});
-
-const OneOutOfThreeSchema = BaseComponent.extend({
-  type: z.literal('oneOutOfThree'),
-  title: z.string(),
-  columns: z.array(OptionSchema),
-  rows: z.array(OptionSchema),
-  required: z.boolean(),
-});
-
-const ComponentSchema = z.discriminatedUnion('type', [
-  RichTextSchema,
-  HeadingSchema,
-  TextInputSchema,
-  SingleChoiceSchema,
-  MultipleChoiceSchema,
-  OneOutOfThreeSchema,
-]);
-export type TLessonComponent = z.infer<typeof ComponentSchema>;
-
 export const ListAssessmentComponentsSuccessResponseSchema = BaseSuccessSchemaFactory(z.object({
-  components: z.array(ComponentSchema),
+  components: z.array(AssessmentComponentSchema),
 }));
 
 export type TListAssessmentComponentsSuccessResponse = z.infer<typeof ListAssessmentComponentsSuccessResponseSchema>;
