c6a3fb2dbde5d6bfcd1e56acfa981b04cd43002e
MytsV <mytsvictoria@gmail.com>
2025-08-11 16:54:34 +0300

    Add lesson components presenter

---
+++ apps/platform/src/lib/infrastructure/client/hooks/use-lesson-components-presenter.ts
@@ -0,0 +1,18 @@
+import { viewModels } from '@maany_shr/e-class-models';
+import { useMemo } from 'react';
+import LessonComponentsPresenter, {
+    TLessonComponentsPresenterUtilities,
+} from '../../common/presenters/lesson-components-presenter';
+
+export function useListLessonComponentsPresenter(
+    setViewModel: (
+        viewModel: viewModels.TLessonComponentListViewModel,
+    ) => void,
+) {
+    const presenterUtilities: TLessonComponentsPresenterUtilities = {};
+    const presenter = useMemo(
+        () => new LessonComponentsPresenter(setViewModel, presenterUtilities),
+        [setViewModel],
+    );
+    return { presenter };
+}
