a54fc75e2f35d3a19704cfc7ccca5195e72932c7
MytsV <mytsvictoria@gmail.com>
2025-08-12 13:50:35 +0300

    Implement outside control of create course form

--- packages/ui-kit/lib/components/create-course-form.tsx
+++ packages/ui-kit/lib/components/create-course-form.tsx
@@ -1,38 +1,90 @@
-import { useState } from 'react'
-import { getDictionary } from '@maany_shr/e-class-translations'
-import { Uploader } from './drag-and-drop-uploader/uploader'
-import { TCourseMetadata, TCourseDuration } from 'packages/models/src/course'
-import { isLocalAware } from '@maany_shr/e-class-translations'
+import { useState } from 'react';
+import { getDictionary } from '@maany_shr/e-class-translations';
+import { Uploader } from './drag-and-drop-uploader/uploader';
+import { isLocalAware } from '@maany_shr/e-class-translations';
 import { fileMetadata } from '@maany_shr/e-class-models';
 import { InputField } from './input-field';
 import RichTextEditor from './rich-text-element/editor';
-import { deserialize } from './rich-text-element/serializer';
+import { serialize } from './rich-text-element/serializer';
 import { Descendant } from 'slate';
-import { Button } from './button';
-import { IconTrashAlt, IconCourse } from './icons';
 import { z } from 'zod';
 
 type TFileMetadataImage = z.infer<typeof fileMetadata.FileMetadataImageSchema>;
 
-
 interface CreateCourseFormProps extends isLocalAware {
     image: TFileMetadataImage | null;
-    onFileChange: (file: fileMetadata.TFileUploadRequest, abortSignal?: AbortSignal) => Promise<fileMetadata.TFileMetadata>;
+    courseTitle: string;
+    setCourseTitle: (title: string) => void;
+    courseSlug: string;
+    setCourseSlug: (slug: string) => void;
+    courseDescription: Descendant[];
+    setCourseDescription: (description: Descendant[]) => void;
+    onFileChange: (
+        file: fileMetadata.TFileUploadRequest,
+        abortSignal?: AbortSignal,
+    ) => Promise<fileMetadata.TFileMetadata>;
     onUploadComplete: (file: TFileMetadataImage) => void;
-    onSave: (course: TCourseMetadata) => void;
     onDelete: (id: string) => void;
     onDownload: (id: string) => void;
 }
 
-export function CreateCourseForm(props: CreateCourseFormProps) {
-    const dictionary = getDictionary(props.locale)
-    const { locale, onSave, onDelete, onDownload } = props;
-    const [courseTitle, setCourseTitle] = useState("");
-    const [courseSlug, setCourseSlug] = useState("");
-    const [courseDescription, setCourseDescription] = useState<Descendant[]>([]);
+export function useCreateCourseForm() {
+    const [courseTitle, setCourseTitle] = useState('');
+    const [courseSlug, setCourseSlug] = useState('');
+    const [courseDescription, setCourseDescription] = useState<Descendant[]>(
+        [],
+    );
+    const [hasUserEditedSlug, setHasEditedCourseSlug] = useState(false);
 
-    const handleOnFilesChange = async (file: fileMetadata.TFileUploadRequest, abortSignal?: AbortSignal) => {
-        return props.onFileChange(file, abortSignal)
+    const serializeDescription = () => serialize(courseDescription);
+
+    const handleCourseSlugChange = (slug: string) => {
+        setCourseSlug(slug);
+        setHasEditedCourseSlug(true);
+    };
+
+    const handleCourseTitleChange = (title: string) => {
+        setCourseTitle(title);
+        if (!hasUserEditedSlug) {
+            const slug = title
+                .toLowerCase()
+                .replace(/[^a-z0-9]+/g, '-')
+                .replace(/^-|-$/g, '');
+            setCourseSlug(slug);
+        }
+    };
+
+    return {
+        courseTitle,
+        setCourseTitle: handleCourseTitleChange,
+        courseSlug,
+        setCourseSlug: handleCourseSlugChange,
+        courseDescription,
+        setCourseDescription,
+        serializeDescription,
+        hasUserEditedSlug,
+    };
+}
+
+export function CreateCourseForm(props: CreateCourseFormProps) {
+    const dictionary = getDictionary(props.locale);
+    const {
+        locale,
+        onDelete,
+        onDownload,
+        courseTitle,
+        setCourseTitle,
+        courseSlug,
+        setCourseSlug,
+        courseDescription,
+        setCourseDescription,
+    } = props;
+
+    const handleOnFilesChange = async (
+        file: fileMetadata.TFileUploadRequest,
+        abortSignal?: AbortSignal,
+    ) => {
+        return props.onFileChange(file, abortSignal);
     };
 
     const handleOnUploadComplete = (file: fileMetadata.TFileMetadata) => {
@@ -41,46 +93,74 @@ export function CreateCourseForm(props: CreateCourseFormProps) {
             return;
         }
         props.onUploadComplete(file);
-    }
+    };
     return (
-        <div className='w-full p-4 bg-card-fill rounded-md flex flex-col gap-4 border-1 border-card-stroke' >
-            <div className='w-full flex flex-col md:flex-row gap-8 min-w-0'>
-                <div className='flex-1 w-full flex flex-col gap-4 min-w-0'>
-                    <div className='flex flex-col gap-1'>
-                        <label className='text-sm md:text-md text-text-secondary'>{dictionary.components.courseIntroInformation.courseTitle}</label>
+        <div className="w-full p-4 bg-card-fill rounded-md flex flex-col gap-4 border-1 border-card-stroke">
+            <div className="w-full flex flex-col md:flex-row gap-8 min-w-0">
+                <div className="flex-1 w-full flex flex-col gap-4 min-w-0">
+                    <div className="flex flex-col gap-1">
+                        <label className="text-sm md:text-md text-text-secondary">
+                            {
+                                dictionary.components.courseIntroInformation
+                                    .courseTitle
+                            }
+                        </label>
                         <InputField
-                            inputText={dictionary.components.courseIntroInformation.courseTitle}
-                            type='text'
-                            value={courseTitle} setValue={value => setCourseTitle(value)} />
+                            inputText={
+                                dictionary.components.courseIntroInformation
+                                    .courseTitle
+                            }
+                            type="text"
+                            value={courseTitle}
+                            setValue={(value) => setCourseTitle(value)}
+                        />
                     </div>
-                    <div className='flex flex-col gap-1'>
-                        <label className='text-sm md:text-md text-text-secondary'>Slug</label>
+                    <div className="flex flex-col gap-1">
+                        <label className="text-sm md:text-md text-text-secondary">
+                            Slug
+                        </label>
                         <InputField
                             inputText="Slug"
-                            type='text'
-                            value={courseSlug} setValue={value => setCourseSlug(value)} />
+                            type="text"
+                            value={courseSlug}
+                            setValue={(value) => setCourseSlug(value)}
+                        />
                     </div>
-                    <div className='flex flex-col gap-1'>
-                        <label className='text-sm md:text-md text-text-secondary'>{dictionary.components.courseIntroInformation.briefDescription}</label>
+                    <div className="flex flex-col gap-1">
+                        <label className="text-sm md:text-md text-text-secondary">
+                            {
+                                dictionary.components.courseIntroInformation
+                                    .briefDescription
+                            }
+                        </label>
                         <RichTextEditor
                             locale={locale}
                             name="courseDescription"
-                            placeholder={dictionary.components.courseIntroInformation.briefDescriptionPlaceholder}
+                            placeholder={
+                                dictionary.components.courseIntroInformation
+                                    .briefDescriptionPlaceholder
+                            }
                             initialValue={courseDescription}
-                            onLoseFocus={(value) => { }}
+                            onLoseFocus={(value) => {}}
                             onChange={(value) => setCourseDescription(value)}
-                            onDeserializationError={(msg, err) => console.error(msg, err)}
+                            onDeserializationError={(msg, err) =>
+                                console.error(msg, err)
+                            }
                         />
-
                     </div>
                 </div>
-                <div className='flex flex-col gap-1 min-w-0 md:w-80 w-full'>
-                    <label className='text-sm md:text-md text-text-secondary leading-[150%] capitalize'>{dictionary.components.courseIntroInformation.featuredImage}</label>
-                    <div className='w-full'>
+                <div className="flex flex-col gap-1 min-w-0 md:w-80 w-full">
+                    <label className="text-sm md:text-md text-text-secondary leading-[150%] capitalize">
+                        {
+                            dictionary.components.courseIntroInformation
+                                .featuredImage
+                        }
+                    </label>
+                    <div className="w-full">
                         <Uploader
                             locale={locale}
-                            variant='image'
-                            type='single'
+                            variant="image"
+                            type="single"
                             file={props.image}
                             onFilesChange={handleOnFilesChange}
                             onUploadComplete={handleOnUploadComplete}
@@ -90,9 +170,6 @@ export function CreateCourseForm(props: CreateCourseFormProps) {
                     </div>
                 </div>
             </div>
-
-
-
         </div>
-    )
+    );
 }
