a15d31effbb53a645bf46f9ccccdfae35776f220
MytsV <mytsvictoria@gmail.com>
2025-08-16 20:52:08 +0300

    Add models for course structure saving

---
+++ apps/platform/src/lib/infrastructure/common/presenters/save-course-structure-presenter.ts
@@ -0,0 +1,100 @@
+import { viewModels, useCaseModels } from '@maany_shr/e-class-models';
+import {
+    BasePresenter,
+    TBaseResponseResponseMiddleware,
+    UnhandledErrorResponse,
+} from '@dream-aim-deliver/dad-cats';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export type TCourseStructurePresenterUtilities = {};
+
+export const SaveCourseStructureResponseMiddleware =
+    {} satisfies TBaseResponseResponseMiddleware<
+        useCaseModels.TSaveCourseStructureUseCaseResponse,
+        viewModels.TSaveCourseStructureViewModel,
+        TCourseStructurePresenterUtilities
+    >;
+
+type TSaveCourseStructureResponseMiddleware =
+    typeof SaveCourseStructureResponseMiddleware;
+
+export default class SaveCourseStructurePresenter extends BasePresenter<
+    useCaseModels.TSaveCourseStructureUseCaseResponse,
+    viewModels.TSaveCourseStructureViewModel,
+    TCourseStructurePresenterUtilities,
+    TSaveCourseStructureResponseMiddleware
+> {
+    constructor(
+        setViewModel: (
+            viewModel: viewModels.TSaveCourseStructureViewModel,
+        ) => void,
+        viewUtilities: TCourseStructurePresenterUtilities,
+    ) {
+        super({
+            schemas: {
+                responseModel:
+                    useCaseModels.SaveCourseStructureUseCaseResponseSchema,
+                viewModel: viewModels.SaveCourseStructureViewModelSchema,
+            },
+            middleware: SaveCourseStructureResponseMiddleware,
+            viewUtilities: viewUtilities,
+            setViewModel: setViewModel,
+        });
+    }
+
+    presentSuccess(
+        response: Extract<
+            useCaseModels.TSaveCourseStructureUseCaseResponse,
+            { success: true }
+        >,
+    ): viewModels.TSaveCourseStructureViewModel {
+        return {
+            mode: 'default',
+            data: {
+                ...response.data,
+            },
+        };
+    }
+
+    presentError(
+        response: UnhandledErrorResponse<
+            useCaseModels.TSaveCourseStructureUseCaseErrorResponse,
+            TSaveCourseStructureResponseMiddleware
+        >,
+    ): viewModels.TSaveCourseStructureViewModel {
+        if (response.data.errorType === 'ConflictError') {
+            return {
+                mode: 'conflict',
+                data: {
+                    type: response.data.errorType,
+                    message: response.data.message,
+                    operation: response.data.operation,
+                    context: response.data.context,
+                    trace: undefined,
+                },
+            };
+        }
+        if (response.data.errorType === 'ValidationError') {
+            return {
+                mode: 'invalid',
+                data: {
+                    type: response.data.errorType,
+                    message: response.data.message,
+                    operation: response.data.operation,
+                    context: response.data.context,
+                    trace: undefined,
+                },
+            };
+        }
+        return {
+            mode: 'kaboom',
+            data: {
+                type: response.data.errorType,
+                message: response.data.message,
+                operation: response.data.operation,
+                context: response.data.context,
+                trace: undefined,
+            },
+        };
+    }
+}
