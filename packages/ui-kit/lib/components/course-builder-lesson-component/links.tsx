609f96537c04e93f6df22d11c9db60207d70b7df
MytsV <mytsvictoria@gmail.com>
2025-08-18 00:12:47 +0300

    Start link designer component implementation

--- packages/ui-kit/lib/components/course-builder-lesson-component/links.tsx
+++ packages/ui-kit/lib/components/course-builder-lesson-component/links.tsx
@@ -1,39 +1,54 @@
 import {
-  CourseElementTemplate,
-  CourseElementType,
-  DesignerComponentProps,
-  FormComponentProps
-} from "../course-builder/types";
-import { getDictionary } from "@maany_shr/e-class-translations";
-import { IconQuiz } from "../icons/icon-quiz";
-import { LinkPreview } from "../links";
+    CourseElementTemplate,
+    CourseElementType,
+    DesignerComponentProps,
+    FormComponentProps,
+} from '../course-builder/types';
+import { getDictionary } from '@maany_shr/e-class-translations';
+import { IconQuiz } from '../icons/icon-quiz';
+import { LinkPreview } from '../links';
+import DesignerLayout from '../designer-layout';
+import { IconLink } from '../icons/icon-link';
 
 const linksElement: CourseElementTemplate = {
-  type: CourseElementType.Quiz,
-  designerBtnElement: {
-    icon: IconQuiz,
-    label: "Quiz"
-  },
-  designerComponent: DesignerComponent,
-  formComponent: FormComponent,
+    type: CourseElementType.Quiz,
+    designerBtnElement: {
+        icon: IconQuiz,
+        label: 'Quiz',
+    },
+    // @ts-ignore
+    designerComponent: DesignerComponent,
+    formComponent: FormComponent,
 };
 
-// TODO: implement designer component for Links
+interface LinkDesignerComponentProps extends DesignerComponentProps {}
+
+// TODO: Implement the designer component for links
 function DesignerComponent({
-  elementInstance,
-  onUpClick,
-  onDownClick,
-  onDeleteClick,
-  locale,
-}: DesignerComponentProps) {
-    return <div>Links Designer Component</div>;
-};
+    elementInstance,
+    onUpClick,
+    onDownClick,
+    onDeleteClick,
+    locale,
+}: LinkDesignerComponentProps) {
+    return (
+        <DesignerLayout
+            type={elementInstance.type}
+            title="Links"
+            icon={<IconLink classNames="w-6 h-6" />}
+            onUpClick={() => onUpClick?.(elementInstance.id)}
+            onDownClick={() => onDownClick?.(elementInstance.id)}
+            onDeleteClick={() => onDeleteClick?.(elementInstance.id)}
+            locale={locale}
+            courseBuilder={true}
+        >
+            <span>To implement</span>
+        </DesignerLayout>
+    );
+}
 
-export function FormComponent({
-  elementInstance,
-  locale,
-}: FormComponentProps) {
-  if (elementInstance.type !== CourseElementType.Links) return null;
+export function FormComponent({ elementInstance, locale }: FormComponentProps) {
+    if (elementInstance.type !== CourseElementType.Links) return null;
 
   return (
     <div className="flex flex-col gap-4 p-4 bg-card-fill border-[1px] border-card-stroke rounded-medium w-full">
