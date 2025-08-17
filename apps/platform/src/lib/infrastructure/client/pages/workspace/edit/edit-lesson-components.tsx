d6d9b711901b8898ad4e7bd715b22c8321226712
MytsV <mytsvictoria@gmail.com>
2025-08-17 21:14:13 +0300

    Add file upload hook

--- apps/platform/src/lib/infrastructure/client/pages/workspace/edit/edit-lesson-components.tsx
+++ apps/platform/src/lib/infrastructure/client/pages/workspace/edit/edit-lesson-components.tsx
@@ -1,15 +1,129 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLessonComponents } from './hooks/edit-lesson-hooks';
 import {
+    AbortError,
+    calculateMd5,
+    downloadFile,
     FormElementType,
     HeadingDesignerComponent,
     HeadingElement,
     LessonElement,
     RichTextDesignerComponent,
     RichTextElement,
+    uploadToS3,
 } from '@maany_shr/e-class-ui-kit';
 import { TLocale } from '@maany_shr/e-class-translations';
 import { useLocale } from 'next-intl';
+import { trpc } from '../../../trpc/client';
+import { fileMetadata } from '@maany_shr/e-class-models';
+
+const useFileUpload = (lessonId: number, componentType: string) => {
+    // TODO: Replace with specific upload implementation
+    const uploadMutation = trpc.uploadCourseImage.useMutation();
+    const verifyMutation = trpc.verifyFile.useMutation();
+
+    const [file, setFile] = useState<fileMetadata.TFileMetadata | null>(null);
+    const [uploadError, setUploadError] = useState<string | undefined>(
+        undefined,
+    );
+
+    const uploadImage = async (
+        uploadRequest: fileMetadata.TFileUploadRequest,
+        abortSignal?: AbortSignal,
+    ) => {
+        if (abortSignal?.aborted) {
+            throw new AbortError();
+        }
+
+        const checksum = await calculateMd5(uploadRequest.file);
+
+        // For mutations, we aren't able to abort them midway.
+        // Hence, we check for abort signal before each step.
+        const uploadResult = await uploadMutation.mutateAsync({
+            // TODO: Pass lessonId and componentType
+            name: uploadRequest.name,
+            checksum,
+            mimeType: uploadRequest.file.type,
+            size: uploadRequest.file.size,
+        });
+        if (!uploadResult.success) {
+            throw new Error('Failed to get upload credentials');
+        }
+
+        if (abortSignal?.aborted) {
+            throw new AbortError();
+        }
+
+        // Comment out to test without the storage running
+        await uploadToS3({
+            file: uploadRequest.file,
+            checksum,
+            storageUrl: uploadResult.data.storageUrl,
+            objectName: uploadResult.data.file.objectName,
+            formFields: uploadResult.data.formFields,
+            abortSignal,
+        });
+
+        const verifyResult = await verifyMutation.mutateAsync({
+            fileId: uploadResult.data.file.id,
+        });
+        if (!verifyResult.success) {
+            throw new Error('Failed to verify image upload');
+        }
+
+        return {
+            id: uploadResult.data.file.id,
+            name: uploadResult.data.file.name,
+            url: verifyResult.data.downloadUrl,
+            thumbnailUrl: verifyResult.data.downloadUrl,
+            size: uploadResult.data.file.size,
+            category: uploadResult.data.file.category,
+        } as fileMetadata.TFileMetadata;
+    };
+
+    const handleFileChange = async (
+        uploadRequest: fileMetadata.TFileUploadRequest,
+        abortSignal?: AbortSignal,
+    ): Promise<fileMetadata.TFileMetadata> => {
+        setUploadError(undefined);
+        try {
+            return await uploadImage(uploadRequest, abortSignal);
+        } catch (error) {
+            if (error instanceof AbortError) {
+                console.warn('File upload was aborted');
+            } else {
+                console.error('File upload failed:', error);
+                // TODO: Translate error message
+                setUploadError('Failed to upload image. Please try again.');
+            }
+            throw error;
+        }
+    };
+
+    const handleUploadComplete = (file: fileMetadata.TFileMetadata) => {
+        setFile(file);
+    };
+
+    const handleDelete = (id: string) => {
+        if (file?.id === id) {
+            setFile(null);
+        }
+    };
+
+    const handleDownload = async (id: string) => {
+        if (file?.id !== id) return;
+        downloadFile(file.url, file.name);
+    };
+
+    return {
+        file,
+        uploadError,
+        handleFileChange,
+        handleUploadComplete,
+        handleDelete,
+        handleDownload,
+    };
+};
 
 interface EditLessonComponentsProps {
     lessonId: number;
