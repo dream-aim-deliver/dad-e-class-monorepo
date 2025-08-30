2ad9bb1cf2904cc4c7f7ee2267b164f4d7d1bd1b
MytsV <mytsvictoria@gmail.com>
2025-08-30 16:38:58 +0300

    Set up error and not found interceptors for wired pages

---
+++ apps/platform/src/app/[locale]/(wired-pages)/not-found.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { getDictionary, TLocale } from '@maany_shr/e-class-translations';
+import { DefaultNotFound } from '@maany_shr/e-class-ui-kit';
+import { useLocale } from 'next-intl';
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+    const locale = useLocale() as TLocale;
+    const dictionary = getDictionary(locale);
+    const router = useRouter();
+    return (
+        <div className="w-full px-30 mb-15">
+        <DefaultNotFound
+            locale={locale}
+            onRetry={() => router.push(`/${locale}`)}
+            buttonLabel={dictionary.components.defaultNotFound.goBack}
+        />
+        </div>
+    );
+}
