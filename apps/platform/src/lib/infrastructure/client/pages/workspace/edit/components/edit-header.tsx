a85e8ce3ee5389b4ada332621909b9b41c4d52eb
MytsV <mytsvictoria@gmail.com>
2025-08-31 21:42:43 +0300

    Fix edit header title

--- apps/platform/src/lib/infrastructure/client/pages/workspace/edit/components/edit-header.tsx
+++ apps/platform/src/lib/infrastructure/client/pages/workspace/edit/components/edit-header.tsx
@@ -17,6 +17,7 @@ interface EditHeaderProps {
 }
 
 export default function EditHeader({
+    title,
     onPreview,
     onSave,
     disablePreview,
@@ -29,14 +30,12 @@ export default function EditHeader({
     const previewButtonText = isPreviewing
         ? dictionary.components.editHeader.hidePreviewText
         : disablePreview
-            ? dictionary.components.editHeader.saveToPreviewText
-            : dictionary.components.editHeader.previewText;
+          ? dictionary.components.editHeader.saveToPreviewText
+          : dictionary.components.editHeader.previewText;
 
     return (
         <div className="flex md:flex-row flex-col md:items-center justify-between gap-5">
-            <h1>
-                {dictionary.components.editHeader.editCourseText}
-            </h1>
+            <h1>{title}</h1>
             <div className="flex sm:flex-row flex-col sm:items-center gap-3">
                 <Button
                     variant="text"
@@ -51,7 +50,11 @@ export default function EditHeader({
                     variant="primary"
                     iconLeft={<IconSave />}
                     hasIconLeft
-                    text={isSaving ? dictionary.components.editHeader.savingText : dictionary.components.editHeader.saveDraftText}
+                    text={
+                        isSaving
+                            ? dictionary.components.editHeader.savingText
+                            : dictionary.components.editHeader.saveDraftText
+                    }
                     onClick={onSave}
                     disabled={isSaving || isPreviewing}
                 />
