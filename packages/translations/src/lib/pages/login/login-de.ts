65bfe86cc5e5b8fc355d9c3f7d71130a2661345f
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-09-06 01:16:15 +0200

    fix(translations): update translations for gender neutrality (German) and informal tone

--- packages/translations/src/lib/pages/login/login-de.ts
+++ packages/translations/src/lib/pages/login/login-de.ts
@@ -2,7 +2,7 @@ import { TDictionary } from "../../dictionaries/base";
 
 export const Login_DE: TDictionary["pages"]["login"] = {
     title: 'Anmelden',
-    postLogout: 'Sie wurden erfolgreich abgemeldet',
+    postLogout: 'Du wurdest erfolgreich abgemeldet', // Guidelines: informal 'Du' instead of formal 'Sie'
     username: 'Benutzername',
     password: 'Passwort',
     signIn: 'Anmelden',
