c55cc5d255096fd3a50a94c88021c18a2a70108e
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-09-09 01:03:50 +0200

    feat(models): add schemas for CourseReview and EClassPackage; implement GetCoursePackages and GetPublicCourseDetails use cases

---
+++ packages/models/src/usecase-models/get-public-course-details-usecase-models.ts
@@ -0,0 +1,59 @@
+import { z } from 'zod';
+import {
+    BaseErrorDiscriminatedUnionSchemaFactory,
+    BaseStatusDiscriminatedUnionSchemaFactory,
+    BaseSuccessSchemaFactory,
+} from '@dream-aim-deliver/dad-cats';
+import { ImageFileSchema } from './common';
+
+export const GetPublicCourseDetailsRequestSchema = z.object({
+    courseSlug: z.string(),
+});
+export type TGetPublicCourseDetailsRequest = z.infer<typeof GetPublicCourseDetailsRequestSchema>;
+
+export const GetPublicCourseDetailsSuccessResponseSchema = BaseSuccessSchemaFactory(z.object({
+    courseVersion: z.number(),
+    title: z.string(),
+    imageFile: ImageFileSchema.nullable(),
+    averageRating: z.number(),
+    reviewCount: z.number().int(),
+    description: z.string(),
+    basePrice: z.number().min(0),
+    priceIncludingCoachings: z.number().min(0),
+    currency: z.string(),
+    duration: z.object({
+        // Minutes
+        video: z.number().int().min(0).nullable(),
+        coaching: z.number().int().min(0).nullable(),
+        selfStudy: z.number().int().min(0).nullable(),
+    }),
+    author: z.object({
+        username: z.string(),
+        name: z.string(),
+        surname: z.string(),
+        averageRating: z.number(),
+        avatarUrl: z.string().nullable()
+    }),
+    coaches: z.array(z.object({
+        name: z.string(),
+        avatarUrl: z.string().nullable()
+    })),
+    students: z.array(z.object({
+        name: z.string(),
+        avatarUrl: z.string().nullable()
+    })),
+    requirements: z.array(z.object({
+        courseName: z.string(),
+        courseSlug: z.string()
+    })),
+}));
+export type TGetPublicCourseDetailsSuccessResponse = z.infer<typeof GetPublicCourseDetailsSuccessResponseSchema>;
+
+const GetPublicCourseDetailsUseCaseErrorResponseSchema = BaseErrorDiscriminatedUnionSchemaFactory({});
+export type TGetPublicCourseDetailsUseCaseErrorResponse = z.infer<typeof GetPublicCourseDetailsUseCaseErrorResponseSchema>;
+
+export const GetPublicCourseDetailsUseCaseResponseSchema = BaseStatusDiscriminatedUnionSchemaFactory([
+    GetPublicCourseDetailsSuccessResponseSchema,
+    GetPublicCourseDetailsUseCaseErrorResponseSchema,
+]);
+export type TGetPublicCourseDetailsUseCaseResponse = z.infer<typeof GetPublicCourseDetailsUseCaseResponseSchema>;
