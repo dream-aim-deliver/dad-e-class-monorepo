a3bb26b87d2fee11c67f4e86185160abeb2beb94
MytsV <mytsvictoria@gmail.com>
2025-09-08 18:51:45 +0300

    Implement layout of pre course assessment form

--- apps/platform/src/lib/infrastructure/client/pages/workspace/edit-pre-course-assessment.tsx
+++ apps/platform/src/lib/infrastructure/client/pages/workspace/edit-pre-course-assessment.tsx
@@ -1,5 +1,21 @@
 'use client';
 
+import { Button, SectionHeading } from '@maany_shr/e-class-ui-kit';
+
 export default function EditPreCourseAssessment() {
-    return <div>Edit Pre-Course Assessment</div>
+    return (
+        <div className="w-full p-4 bg-card-fill rounded-md flex flex-col gap-3 border-1 border-card-stroke">
+            <SectionHeading text="Pre-course assessment form" />
+            <span className="text-sm text-text-secondary">
+                This form is displayed automatically at the beginning of each
+                course
+            </span>
+            <div className="flex flex-col justify-between w-full bg-neutral-800 h-28 rounded-md border-1 border-neutral-700 p-4">
+                <span className="text-sm text-text-primary font-bold">
+                    This form is disabled.
+                </span>
+                <Button variant="primary" text="Enable form" className="w-min" size="small" />
+            </div>
+        </div>
+    );
 }
