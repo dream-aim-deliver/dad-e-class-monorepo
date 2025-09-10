d3289f61f7ca9c2eb9681c43a7f474002c943867
jatinrise <godnanijatin@gmail.com>
2025-09-10 19:23:12 +0530

    refactor: Remove console log statements for cleaner code and improved readability

--- apps/platform/src/lib/infrastructure/common/mocks/procedures/course-details.ts
+++ apps/platform/src/lib/infrastructure/common/mocks/procedures/course-details.ts
@@ -10,7 +10,6 @@ export const saveCourseDetails = t.procedure
         async (
             ctx,
         ): Promise<useCaseModels.TSaveCourseDetailsUseCaseResponse> => {
-            console.log(ctx.input);
             await new Promise((resolve) => setTimeout(resolve, 1000));
             return {
                 success: true,
