fc101feb7622165a839f346ec7f16356d471c70b
MytsV <mytsvictoria@gmail.com>
2025-09-11 19:38:05 +0300

    Connect home page and offers page

--- apps/platform/src/lib/infrastructure/client/pages/offers/offers.tsx
+++ apps/platform/src/lib/infrastructure/client/pages/offers/offers.tsx
@@ -9,7 +9,7 @@ import {
     Divider,
     Outline,
 } from '@maany_shr/e-class-ui-kit';
-import { trpc } from '../../trpc/client';
+import { trpc } from '../../trpc/cms-client';
 import { Suspense, lazy, useState } from 'react';
 import { viewModels } from '@maany_shr/e-class-models';
 import { useGetOffersPageOutlinePresenter } from '../../hooks/use-offers-page-outline-presenter';
@@ -27,80 +27,70 @@ interface OffersProps {
 }
 
 export default function Offers(props: OffersProps) {
+    // Data fetching and presentation logic
+    const [outlineResponse] = trpc.getOffersPageOutline.useSuspenseQuery({});
+    const [outlineViewModel, setOutlineViewModel] = useState<
+        viewModels.TOffersPageOutlineViewModel | undefined
+    >(undefined);
+
+    const { presenter } = useGetOffersPageOutlinePresenter(setOutlineViewModel);
+    // @ts-ignore
+    presenter.present(outlineResponse, outlineViewModel);
+
+    const locale = useLocale() as TLocale;
+    const t = useTranslations('pages.offers');
+
+    // Filter
     const [selectedTopics, setSelectedTopics] = useState<string[]>(
         props.initialSelectedTopics ?? [],
     );
     const [coachingIncluded, setCoachingIncluded] = useState<boolean>(false);
 
-    return <OffersCourseList
+    if (!outlineViewModel) {
+        return <DefaultLoading locale={locale} variant="minimal" />;
+    }
+
+    if (outlineViewModel.mode === 'kaboom') {
+        return <DefaultError locale={locale} />;
+    }
+
+    const outline = outlineViewModel.data;
+
+    return (
+        <div className="flex flex-col space-y-5 px-30">
+            <Outline title={outline.title} description={outline.description} />
+            <OffersFilters
+                selectedTopics={selectedTopics}
+                setSelectedTopics={setSelectedTopics}
+            />
+            <Divider className="my-12" />
+            <OffersCourseHeading
+                coachingIncluded={coachingIncluded}
+                setCoachingIncluded={setCoachingIncluded}
+            />
+            <Suspense fallback={<CourseCardListSkeleton />}>
+                <OffersCourseList
                     selectedTopics={selectedTopics}
                     coachingIncluded={coachingIncluded}
-                />;
-
-    // Data fetching and presentation logic
-    // const [outlineResponse] = trpc.getOffersPageOutline.useSuspenseQuery({});
-    // const [outlineViewModel, setOutlineViewModel] = useState<
-    //     viewModels.TOffersPageOutlineViewModel | undefined
-    // >(undefined);
-
-    // const { presenter } = useGetOffersPageOutlinePresenter(setOutlineViewModel);
-    // presenter.present(outlineResponse, outlineViewModel);
-
-    // const locale = useLocale() as TLocale;
-    // const t = useTranslations('pages.offers');
-
-    // // Filter
-    // const [selectedTopics, setSelectedTopics] = useState<string[]>(
-    //     props.initialSelectedTopics ?? [],
-    // );
-    // const [coachingIncluded, setCoachingIncluded] = useState<boolean>(false);
-
-    // if (!outlineViewModel) {
-    //     return <DefaultLoading locale={locale} variant="minimal" />;
-    // }
-
-    // if (outlineViewModel.mode === 'kaboom') {
-    //     return <DefaultError locale={locale} />;
-    // }
-
-    // const outline = outlineViewModel.data;
-
-    // return (
-    //     <div className="flex flex-col space-y-5 px-30">
-    //         <Outline title={outline.title} description={outline.description} />
-    //         <h2> {t('chooseCategory')} </h2>
-    //         <OffersFilters
-    //             selectedTopics={selectedTopics}
-    //             setSelectedTopics={setSelectedTopics}
-    //         />
-    //         <Divider className="my-12" />
-    //         <OffersCourseHeading
-    //             coachingIncluded={coachingIncluded}
-    //             setCoachingIncluded={setCoachingIncluded}
-    //         />
-    //         <Suspense fallback={<CourseCardListSkeleton />}>
-    //             <OffersCourseList
-    //                 selectedTopics={selectedTopics}
-    //                 coachingIncluded={coachingIncluded}
-    //             />
-    //         </Suspense>
-    //         <Divider className="my-12" />
-    //         <h2> {t('ourPackages')} </h2>
-    //         <Suspense
-    //             fallback={<DefaultLoading locale={locale} variant="minimal" />}
-    //         >
-    //             <PackageList />
-    //         </Suspense>
-    //         <Divider className="my-12" />
-    //         <h2> {t('coachingOnDemand')} </h2>
-    //         <Suspense fallback={<CoachCardListSkeleton />}>
-    //             <OffersCoachList selectedTopics={selectedTopics} />
-    //         </Suspense>
-    //         <Divider className="my-12" />
-    //         <h2> {t('haveNotFound')} </h2>
-    //         <Suspense fallback={<CarouselSkeleton />}>
-    //             <Carousel />
-    //         </Suspense>
-    //     </div>
-    // );
+                />
+            </Suspense>
+            <Divider className="my-12" />
+            <h2> {t('ourPackages')} </h2>
+            <Suspense
+                fallback={<DefaultLoading locale={locale} variant="minimal" />}
+            >
+                <PackageList />
+            </Suspense>
+            <Divider className="my-12" />
+            <h2> {t('coachingOnDemand')} </h2>
+            <Suspense fallback={<CoachCardListSkeleton />}>
+                <OffersCoachList selectedTopics={selectedTopics} />
+            </Suspense>
+            <Divider className="my-12" />
+            <h2> {t('haveNotFound')} </h2>
+            <Suspense fallback={<CarouselSkeleton />}>
+                <Carousel items={outline.items} />
+            </Suspense>
+        </div>
+    );
 }
