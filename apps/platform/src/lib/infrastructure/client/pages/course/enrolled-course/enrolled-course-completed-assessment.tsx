b9b4d170b785a63847261799f93272f17ae4838c
MytsV <mytsvictoria@gmail.com>
2025-09-16 19:52:44 +0300

    Connect assessment form and pre course assessment progress

--- apps/platform/src/lib/infrastructure/client/pages/course/enrolled-course/enrolled-course-completed-assessment.tsx
+++ apps/platform/src/lib/infrastructure/client/pages/course/enrolled-course/enrolled-course-completed-assessment.tsx
@@ -1,6 +1,5 @@
 import { viewModels } from '@maany_shr/e-class-models';
 import { trpc } from '../../../trpc/client';
-import { useListAssessmentComponentsPresenter } from '../../../hooks/use-assessment-components-presenter';
 import { useMemo, useState } from 'react';
 import {
     DefaultError,
@@ -8,7 +7,7 @@ import {
     SubmissionRenderer,
     FormElement,
 } from '@maany_shr/e-class-ui-kit';
-import { transformLessonComponentsWithProgress } from '../../../utils/transform-lesson-components';
+import { transformLessonComponents } from '../../../utils/transform-lesson-components';
 import { useLocale } from 'next-intl';
 import { TLocale } from '@maany_shr/e-class-translations';
 import { useListAssessmentProgressesPresenter } from '../../../hooks/use-assessment-progresses-presenter';
@@ -22,14 +21,6 @@ export default function EnrolledCourseCompletedAssessment(
 ) {
     const locale = useLocale() as TLocale;
 
-    const [componentsResponse] = trpc.listPreCourseAssessmentComponents.useSuspenseQuery({});
-    const [componentsViewModel, setComponentsViewModel] = useState<
-        viewModels.TAssessmentComponentListViewModel | undefined
-    >(undefined);
-    const { presenter: assessmentsPresenter } =
-        useListAssessmentComponentsPresenter(setComponentsViewModel);
-    assessmentsPresenter.present(componentsResponse, componentsViewModel);
-
     const [progressResponse] = trpc.listAssessmentProgresses.useSuspenseQuery({
         courseSlug: props.courseSlug,
     });
@@ -41,30 +32,20 @@ export default function EnrolledCourseCompletedAssessment(
     progressPresenter.present(progressResponse, progressViewModel);
 
     const formElements: FormElement[] = useMemo(() => {
-        if (!componentsViewModel || componentsViewModel.mode !== 'default') {
-            return [];
-        }
         if (!progressViewModel || progressViewModel.mode !== 'default') {
             return [];
         }
 
-        const components = componentsViewModel.data.components;
-        const progress = progressViewModel.data.progress;
+        const components = progressViewModel.data.components;
 
-        return transformLessonComponentsWithProgress(
-            components,
-            progress,
-        ) as FormElement[];
-    }, [componentsViewModel, progressViewModel]);
+        return transformLessonComponents(components) as FormElement[];
+    }, [progressViewModel]);
 
-    if (!componentsViewModel || !progressViewModel) {
+    if (!progressViewModel) {
         return <DefaultLoading locale={locale} variant="minimal" />;
     }
 
-    if (
-        componentsViewModel.mode === 'kaboom' ||
-        progressViewModel.mode === 'kaboom'
-    ) {
+    if (progressViewModel.mode === 'kaboom') {
         return <DefaultError locale={locale} />;
     }
 
