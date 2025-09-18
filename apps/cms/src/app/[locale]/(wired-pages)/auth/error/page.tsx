576bde4486f980ce2b6b617910ffd727cb13b5d5
maany <imptodefeat@gmail.com>
2025-09-18 15:25:41 +0200

    refactor: update import paths and enhance middleware session handling with debug logs

--- apps/cms/src/app/[locale]/(wired-pages)/auth/error/page.tsx
+++ apps/cms/src/app/[locale]/(wired-pages)/auth/error/page.tsx
@@ -1,4 +1,4 @@
-import LoginErrorServerComponent from '../../../../lib/infrastructure/server/pages/auth/login-error-rsc';
+import LoginErrorServerComponent from '../../../../../lib/infrastructure/server/pages/auth/login-error-rsc';
 
 export default async function Login() {
     return <LoginErrorServerComponent />;
