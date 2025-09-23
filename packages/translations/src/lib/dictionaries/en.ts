4ff19f0124d3d3da29112637e2f86fb4379b3654
Carla Bordo <carlapbg@gmail.com>
2025-09-24 00:55:31 +0200

    feat(teach-course-banner): remove title and description from dictionary, add props for customizable title and description

--- packages/translations/src/lib/dictionaries/en.ts
+++ packages/translations/src/lib/dictionaries/en.ts
@@ -1089,8 +1089,6 @@ export const EN: TDictionary = {
       emptyState: 'No platforms yet.',
     },
     teachCourseBanner: {
-      title: 'Turn Your Expertise Into Earnings',
-      description: 'Have the skills? Become a coach and inspire others! Share your knowledge, teach this course, and earn while making a difference. Join our community of experts.',
       placeHolderText: 'Image not available',
       teachButtonText: 'Teach this course',
     },
