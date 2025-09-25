ebc620e1d33c7df8161279993e234a2a7533638a
maany <imptodefeat@gmail.com>
2025-09-25 15:51:28 +0200

    feat: update CI and publish workflows with S3 configuration and remove unused topics functionality

--- apps/cms/src/lib/infrastructure/client/pages/list-platforms.tsx
+++ apps/cms/src/lib/infrastructure/client/pages/list-platforms.tsx
@@ -14,10 +14,7 @@ import { TLocale } from '@maany_shr/e-class-translations';
 import dynamic from 'next/dynamic';
 import { viewModels } from '@maany_shr/e-class-models';
 import { useState } from 'react';
-import { useGetHomePagePresenter } from '../hooks/use-home-page-presenter';
-import { useListTopicsPresenter } from '../hooks/use-topics-presenter';
 import { useListPlatformsPresenter } from '../hooks/use-list-platforms-presenter';
-import env from '../config/env';
 
 const Carousel = dynamic(
     () =>
@@ -30,38 +27,6 @@ const Carousel = dynamic(
     },
 );
 
-function Topics() {
-    const locale = useLocale() as TLocale;
-    const t = useTranslations('pages.home');
-
-    const [topicsResponse, { refetch }] = trpc.listTopics.useSuspenseQuery({});
-    const [topicsViewModel, setTopicsViewModel] = useState<
-        viewModels.TTopicListViewModel | undefined
-    >(undefined);
-    const { presenter: topicsPresenter } =
-        useListTopicsPresenter(setTopicsViewModel);
-    // @ts-ignore
-    topicsPresenter.present(topicsResponse, topicsViewModel);
-
-    if (!topicsViewModel) {
-        return <DefaultLoading locale={locale} variant="minimal" />;
-    }
-
-    if (topicsViewModel.mode === 'kaboom') {
-        return (
-            <DefaultError
-                locale={locale}
-                onRetry={() => {
-                    refetch();
-                }}
-            />
-        );
-    }
-
-    const topics = topicsViewModel.data.topics;
-
-    return <TopicList list={topics} title={t('topicsTitle')} />;
-}
 
 export default function ListPlatforms() {
     const locale = useLocale() as TLocale;
