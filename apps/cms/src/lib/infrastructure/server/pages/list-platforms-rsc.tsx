b5890a72e6d4ba97e3c71b9f7afa1957488b2d36
maany <imptodefeat@gmail.com>
2025-09-25 10:45:10 +0200

    feat(cms): refactor platform management components and implement platform-aware TRPC client

--- apps/cms/src/lib/infrastructure/server/pages/list-platforms-rsc.tsx
+++ apps/cms/src/lib/infrastructure/server/pages/list-platforms-rsc.tsx
@@ -1,9 +1,10 @@
 import ListPlatforms from '../../client/pages/list-platforms';
 import { Suspense } from 'react';
 import DefaultLoadingWrapper from '../../client/wrappers/default-loading';
-import { HydrateClient, prefetch, trpc } from '../config/trpc/cms-server';
+import { HydrateClient, prefetch, getServerTRPC } from '../config/trpc/cms-server';
 
 export default async function ListPlatformsServerComponent() {
+    // const trpc = getServerTRPC();
     // await Promise.all([
     //     prefetch(trpc.getHomePage.queryOptions({})),
     //     prefetch(trpc.listTopics.queryOptions({})),
