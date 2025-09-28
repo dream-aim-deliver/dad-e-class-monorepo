6e7abd7f5583fecbffb2ab60a9ceb1a84aa48c6b
MytsV <mytsvictoria@gmail.com>
2025-09-28 13:49:21 +0300

    Simplify coaching session structure

--- packages/models/src/usecase-models/get-coach-availability-usecase-models.ts
+++ packages/models/src/usecase-models/get-coach-availability-usecase-models.ts
@@ -17,35 +17,14 @@ export const CoachingStatusSchema = z.enum(['unscheduled', 'requested', 'accepte
 export type TCoachingStatus = z.infer<typeof CoachingStatusSchema>;
 
 export const GetCoachAvailabilitySuccessResponseSchema = BaseSuccessSchemaFactory(z.object({
-  availability: z.discriminatedUnion('type', [
-    z.object({
-      type: z.literal('single'),
-      startTime: z.string().datetime(),
-      endTime: z.string().datetime(),
-    }),
-    z.object({
-      type: z.literal('recurring'),
-      startTime: z.string().datetime(),
-      endTime: z.string().datetime(),
-      monday: z.boolean(),
-      tuesday: z.boolean(), 
-      wednesday: z.boolean(),
-      thursday: z.boolean(),
-      friday: z.boolean(),
-      saturday: z.boolean(),
-      sunday: z.boolean(),
-      createdAt: z.string().datetime(),
-      expirationDate: z.string().datetime(),
-    })
-  ]).array(),
+  availability: z.array(z.object({
+    startTime: z.string().datetime(),
+    endTime: z.string().datetime(),
+  })),
   mySessions: z.array(z.object({
     id: z.number(),
     status: CoachingStatusSchema,
     coachingOfferingName: z.string(),
-    startTime: z.string().datetime(),
-    endTime: z.string().datetime(),
-  })),
-  anonymousSessions: z.array(z.object({
     startTime: z.string().datetime(),
     endTime: z.string().datetime(),
   })),
