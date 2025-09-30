d6422a3e85ea6afae892c75a3f453e93951cca2c
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-10-01 01:48:26 +0200

    chore(cms): remove boughtCoachingIds from available coachings mock data

--- apps/cms/src/lib/infrastructure/common/mocks/procedures/available-coachings.ts
+++ apps/cms/src/lib/infrastructure/common/mocks/procedures/available-coachings.ts
@@ -8,25 +8,21 @@ const availableCoachings: useCaseModels.TListAvailableCoachingsSuccessResponse['
                 id: 1,
                 name: 'Quick feedback',
                 duration: 10,
-                boughtCoachingIds: [1, 2],
             },
             {
                 id: 2,
                 name: 'Quick sprint',
                 duration: 20,
-                boughtCoachingIds: [3, 4, 5],
             },
             {
                 id: 4,
                 name: 'Full immersion',
                 duration: 60,
-                boughtCoachingIds: [6],
             },
             {
                 id: 5,
                 name: '1/2 day workshop',
                 duration: 240,
-                boughtCoachingIds: [7],
             },
         ],
     };
@@ -38,25 +34,21 @@ const availableCoachingsGerman: useCaseModels.TListAvailableCoachingsSuccessResp
                 id: 1,
                 name: 'Schnelle Rückmeldung',
                 duration: 10,
-                boughtCoachingIds: [1, 2],
             },
             {
                 id: 2,
                 name: 'Schneller Sprint',
                 duration: 20,
-                boughtCoachingIds: [3, 4, 5],
             },
             {
                 id: 4,
                 name: 'Volles Eintauchen',
                 duration: 60,
-                boughtCoachingIds: [6],
             },
             {
                 id: 5,
                 name: '1/2-tägiger Workshop',
                 duration: 240,
-                boughtCoachingIds: [7],
             },
         ],
     };
