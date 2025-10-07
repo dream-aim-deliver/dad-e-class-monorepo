f3f933ae3013ec6865c9de0064afe177ebd3179c
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-10-07 04:52:22 +0200

    feat(presenters): implement request file upload presenter and view model in platform and cms

--- packages/models/src/view-models/index.ts
+++ packages/models/src/view-models/index.ts
@@ -75,3 +75,5 @@ export * from './request-coaching-session-view-model';
 export * from './unschedule-coaching-session-view-model';
 export * from './create-notification-view-model';
 export * from './assignment-view-model';
+
+export * from './request-file-upload-view-model';
