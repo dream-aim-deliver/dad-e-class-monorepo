7b32c0210d6df6c457c3c1606dac0754cab52ce7
manishRise11 <manish.k@rise11.com>
2025-10-08 15:04:34 +0530

    feat(interaction-card): add CoachStudentInteractionCard component with nested accordion for modules and lessons

---
+++ packages/ui-kit/lib/components/coach-student-interaction-card.tsx
@@ -0,0 +1,187 @@
+import { getDictionary, isLocalAware, TLocale } from '@maany_shr/e-class-translations';
+import { TListStudentInteractionsSuccess } from 'packages/models/src/view-models';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from './accordion';
+import { cn } from '../utils/style-utils';
+import { IconChevronDown, IconChevronUp, IconTextInput } from './icons';
+import { Button } from './button';
+import RichTextRenderer from './rich-text-element/renderer';
+
+interface CoachStudentInteractionCardProps extends isLocalAware {
+  onViewLessonsClick: (moduleId: string, lessonId: string) => void;
+  locale: TLocale;
+  modules: TListStudentInteractionsSuccess['modules'];
+}
+
+export const  CoachStudentInteractionCard = ({
+  modules,
+  locale,
+  onViewLessonsClick,
+}: CoachStudentInteractionCardProps) => {
+  const dictionary = getDictionary(locale);
+  return (
+    <Accordion
+      className={cn("flex flex-col gap-4")}
+      type="multiple"
+      defaultValue={modules.length > 0 ? [`module-${modules[0].id}`] : []}
+    >
+      {/* Map through all modules */}
+      {modules.map((module) => (
+        <AccordionItem
+          key={`module-${module.id}`}
+          value={`module-${module.id}`}
+          className="bg-card-fill border border-divider px-4 py-6 rounded-medium"
+        >
+          <AccordionTrigger
+            value={`module-${module.id}`}
+            className="w-full"
+            expandIcon={
+              <span
+                title={
+                  dictionary.components
+                    .courseMaterialsAccordion.expand
+                }
+                className="text-button-text-text"
+              >
+                <IconChevronUp size="6" />
+              </span>
+            }
+            collapseIcon={
+              <span
+                title={
+                  dictionary.components
+                    .courseMaterialsAccordion.collapse
+                }
+                className="text-button-text-text"
+              >
+                <IconChevronDown size="6" />
+              </span>
+            }
+          >
+            <h4 className="text-text-primary md:text-xl text-md font-bold">{dictionary.components.courseMaterialsAccordion.module} {module.position}: {module.title}</h4>
+          </AccordionTrigger>
+
+          <AccordionContent value={`module-${module.id}`} className="pt-4 pb-6">
+            {/* Nested Lessons Accordion */}
+            {/* <hr className="flex-grow border-t border-divider mb-4" /> */}
+
+            <div className="pb-2">
+              <Accordion
+                type="multiple"
+                className="flex flex-col gap-4"
+              >
+                {module.lessons?.map((lesson) => (
+                  <AccordionItem
+                    key={`lesson-${lesson.id}`}
+                    value={`lesson-${lesson.id}`}
+                    className="bg-base-neutral-800 p-4 rounded-medium border border-base-neutral-700"
+                  >
+                    <AccordionTrigger
+                      value={`lesson-${lesson.id}`}
+                      className="w-full"
+                      expandIcon={
+                        <span
+                          title={
+                            dictionary.components
+                              .courseMaterialsAccordion.expand
+                          }
+                          className="text-button-text-text"
+                        >
+                          <IconChevronUp size="6" />
+                        </span>
+                      }
+                      collapseIcon={
+                        <span
+                          title={
+                            dictionary.components
+                              .courseMaterialsAccordion.collapse
+                          }
+                          className="text-button-text-text"
+                        >
+                          <IconChevronDown size="6" />
+                        </span>
+                      }
+                    >
+                      <div className="flex items-center justify-between gap-4 flex-1">
+                        <div className="flex items-center gap-4">
+                          <h5 className="text-text-primary md:text-lg text-sm font-bold">{dictionary.components.courseMaterialsAccordion.lesson} {lesson.position}: {lesson.title}</h5>
+                        </div>
+                        <Button
+                          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                            e.stopPropagation();
+                            onViewLessonsClick(module.id, lesson.id);
+                          }}
+                          variant='text'
+                          text='View Lesson'
+                        />
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent value={`lesson-${lesson.id}`} className="pb-4">
+                      {/* Student-Coach Interactions */}
+                      <div className="flex flex-col gap-4 mt-4">
+                        <hr className="flex-grow border-t border-divider" />
+                        {lesson.textInputs?.length > 0 ? (
+                          lesson.textInputs.map((input, index) => (
+                            <div
+                              key={`text-input-${index}`}
+                              className="w-full p-4 bg-base-neutral-800 rounded-medium border border-base-neutral-700"
+                            >
+                              {/* Question Section */}
+                              <div className="flex flex-col gap-2 items-start">
+                                <Button
+                                  hasIconLeft
+                                  iconLeft={<IconTextInput />}
+                                  text="Text Input"
+                                  variant="text"
+                                  className="p-0 text-base-white hover:text-base-white"
+                                />
+                                <RichTextRenderer
+                                  content={input?.helperText}
+                                  className="text-text-secondary"
+                                  onDeserializationError={(message: string, error: Error) => {
+                                    console.error('Deserialization error:', message, error);
+                                  }}
+                                />
+                              </div>
+
+                              <hr className="my-4 border-t border-divider" />
+
+                              {/* Student Answer Section */}
+                              <div className="flex flex-col gap-2 items-start">
+                                <Button
+                                 
+                                  text="Student Reply"
+                                  variant="text"
+                                  className="p-0 text-base-white hover:text-base-white"
+                                />
+                                <RichTextRenderer
+                                  content={input?.progress?.answer ?? ''}
+                                  className="text-text-secondary"
+                                  onDeserializationError={(message: string, error: Error) => {
+                                    console.error('Deserialization error:', message, error);
+                                  }}
+                                />
+                              </div>
+                            </div>
+                          ))
+                        ) : (
+                          <p className="text-text-secondary text-sm">
+                            {dictionary.pages.student.noInteractionsYet}
+                          </p>
+                        )}
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
