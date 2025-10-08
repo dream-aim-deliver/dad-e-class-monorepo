f08954695081579f0cc101f0909df0f67bb32b74
MytsV <mytsvictoria@gmail.com>
2025-10-08 22:45:56 +0300

    Improve mobile coaching availability

--- packages/ui-kit/lib/components/available-coaching-sessions/coaching-availability-card.tsx
+++ packages/ui-kit/lib/components/available-coaching-sessions/coaching-availability-card.tsx
@@ -1,35 +1,63 @@
 import { isLocalAware } from '@maany_shr/e-class-translations';
 import { Button } from '../button';
 
-interface CoachingAvailabilityCardProps extends isLocalAware {
+interface RequestCoachingAvailabilityCardProps extends isLocalAware {
     startTime: Date;
     endTime: Date;
     onClick: () => void;
 }
 
-export function CoachingAvailabilityCard({
+function formatTime(date: Date) {
+    return date.toLocaleTimeString(undefined, {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+}
+
+export function RequestCoachingAvailabilityCard({
     startTime,
     endTime,
     onClick,
-}: CoachingAvailabilityCardProps) {
-    const formatDate = (date: Date) => {
-        return date.toLocaleTimeString(undefined, {
-            hour: '2-digit',
-            minute: '2-digit',
-        });
-    };
-
+}: RequestCoachingAvailabilityCardProps) {
     return (
         <div className="bg-card-fill rounded-md p-4 flex flex-row justify-between items-center border border-card-stroke">
             <div>
-                <div className="text-text-secondary text-sm">
-                    Availability
-                </div>
+                <div className="text-text-secondary text-sm">Availability</div>
                 <div className="text-text-primary">
-                    {formatDate(startTime)} - {formatDate(endTime)}
+                    {formatTime(startTime)} - {formatTime(endTime)}
                 </div>
             </div>
             <Button variant="primary" text="Request" onClick={onClick} />
+        </div>
+    );
+}
+
+interface OwnerCoachingAvailabilityCardProps extends isLocalAware {
+    availability: {
+        startTime: Date;
+        endTime: Date;
+        onClick?: () => void;
+    };
+    coachingSessions: {
+        startTime: Date;
+        endTime: Date;
+        onClick?: () => void;
+        title: string;
+    }[];
+}
+
+export function OwnerCoachingAvailabilityCard({
+    availability,
+    coachingSessions,
+}: OwnerCoachingAvailabilityCardProps) {
+    return (
+        <div className="bg-card-fill rounded-md p-4 flex flex-row justify-between items-center border border-card-stroke">
+            <div className="bg-brand-500">
+                <div className="text-text-primary">
+                    {formatTime(availability.startTime)} -{' '}
+                    {formatTime(availability.endTime)}
+                </div>
+            </div>
         </div>
     );
 }
