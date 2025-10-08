4a9d1c37d79f821695ebea8313288ed49a39a454
MytsV <mytsvictoria@gmail.com>
2025-10-09 00:55:36 +0300

    Implement blocking seeing your own booking

--- apps/platform/src/lib/infrastructure/server/pages/booking-rsc.tsx
+++ apps/platform/src/lib/infrastructure/server/pages/booking-rsc.tsx
@@ -1,14 +1,27 @@
 import { Suspense } from 'react';
 import BookCoachPage from '../../client/pages/booking/book-coach';
 import DefaultLoadingWrapper from '../../client/wrappers/default-loading';
+import getSession from '../config/auth/get-session';
+import { redirect } from 'next/navigation';
 
 interface BookingServerComponentProps {
     coachUsername: string;
 }
 
-export default function BookingServerComponent(
+export default async function BookingServerComponent(
     props: BookingServerComponentProps,
 ) {
+    const session = await getSession();
+
+    if (!session || !session.user) {
+        redirect('/auth/login');
+    }
+
+    if (session.user.name === props.coachUsername) {
+        // TODO: fill in localized error message
+        throw new Error('You cannot book a session with yourself');
+    }
+
     return (
         <Suspense fallback={<DefaultLoadingWrapper />}>
             <BookCoachPage coachUsername={props.coachUsername} />
