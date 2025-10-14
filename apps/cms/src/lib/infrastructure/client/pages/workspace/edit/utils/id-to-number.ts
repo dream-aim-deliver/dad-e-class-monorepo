91a8fa5b2359a594b12ef012952cb272f0f9433a
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2025-10-15 01:45:03 +0200

    chore(cms): remove dead code to fix build issues while democking

--- apps/cms/src/lib/infrastructure/client/pages/workspace/edit/utils/id-to-number.ts
+++
@@ -1,5 +0,0 @@
-export function idToNumber(id: string | undefined): number | undefined {
-    if (id === undefined) return undefined;
-    const parsed = parseInt(id, 10);
-    return isNaN(parsed) ? undefined : parsed;
-}
