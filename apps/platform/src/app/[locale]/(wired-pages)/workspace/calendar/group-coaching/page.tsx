5f63dddfb2a8533bdccb25f665b379bb16a3f24f
Divyanshu Gautam <divyanshu@rise11.com>
2025-11-07 17:04:07 +0530

    refactor: remove unused group coaching and calendar server components

--- apps/platform/src/app/[locale]/(wired-pages)/workspace/calendar/group-coaching/page.tsx
+++
@@ -1,5 +0,0 @@
-import GroupCoachingCalendarServerComponent from '../../../../../../lib/infrastructure/server/pages/workspace/group-coaching-calendar-rsc';
-
-export default async function Page() {
-    return <GroupCoachingCalendarServerComponent />;
-}
