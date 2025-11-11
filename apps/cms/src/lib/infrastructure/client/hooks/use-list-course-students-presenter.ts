9b9f5c889f2554e45e6e31efa3a684db1085133a
Divyanshu Gautam <divyanshu@rise11.com>
2025-11-11 18:18:24 +0530

    refactor: update ListCourseStudentsPresenter to use new view models and utilities

--- apps/cms/src/lib/infrastructure/client/hooks/use-list-course-students-presenter.ts
+++ apps/cms/src/lib/infrastructure/client/hooks/use-list-course-students-presenter.ts
@@ -1,14 +1,14 @@
 import { viewModels } from '@maany_shr/e-class-models';
 import { useMemo } from 'react';
 import ListCourseStudentsPresenter, { 
-    TCourseStudentsPresenterUtilities 
+    TListCourseStudentsPresenterUtilities 
 } from '../../common/presenters/list-course-students-presenter';
 
 
 export function useListCourseStudentsPresenter(
-    setViewModel: (viewModel: viewModels.TCourseStudentsListViewModel) => void,
+    setViewModel: (viewModel: viewModels.TListCourseStudentsViewModel) => void,
 ) {
-    const presenterUtilities: TCourseStudentsPresenterUtilities = {};
+    const presenterUtilities: TListCourseStudentsPresenterUtilities = {};
     const presenter = useMemo(
         () => new ListCourseStudentsPresenter(setViewModel, presenterUtilities),
         [setViewModel],
