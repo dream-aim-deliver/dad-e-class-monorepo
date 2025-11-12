d9293b16323fff63191e8cba5c07f118872551c4
Divyanshu Gautam <divyanshu@rise11.com>
2025-11-12 22:40:58 +0530

    refactor: replace TAppRouter with MockRouter in TRPC client setup

--- apps/platform/src/lib/infrastructure/client/trpc/client.ts
+++ apps/platform/src/lib/infrastructure/client/trpc/client.ts
@@ -1,5 +1,4 @@
-import { TAppRouter } from '@dream-aim-deliver/e-class-cms-rest';
+import { MockRouter } from '../../common/mocks/mock-router';
 import { createTRPCReact } from '@trpc/react-query';
-import type { CreateTRPCReact } from '@trpc/react-query';
 
-export const trpc: CreateTRPCReact<TAppRouter, unknown> = createTRPCReact<TAppRouter>();
+export const trpc = createTRPCReact<MockRouter>();
