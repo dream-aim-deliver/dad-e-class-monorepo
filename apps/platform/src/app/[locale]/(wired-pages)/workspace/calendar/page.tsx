f285e71c54a004b7a9e3025f12a635fcc24222bb
Divyanshu Gautam <divyanshu@rise11.com>
2025-11-20 10:25:01 +0530

    Revert "refactor: remove unused group coaching and calendar server components"
    This reverts commit a8a02c93da01b86ead73ed3eb9377566315a5d00.

---
+++ apps/platform/src/app/[locale]/(wired-pages)/workspace/calendar/page.tsx
@@ -0,0 +1,5 @@
+import CalendarServerComponent from '../../../../../lib/infrastructure/server/pages/workspace/calendar-rsc';
+
+export default async function Page() {
+    return <CalendarServerComponent />;
+}
