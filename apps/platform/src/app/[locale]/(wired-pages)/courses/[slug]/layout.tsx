f414f0b59d4f1dcca1d8f3e4edd764e8c559b315
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2026-02-05 16:29:59 +0100

    fix: remove SidebarLayout from Course, Student, and Workspace layouts for simplified rendering; add it in the general layout instead to be shown everywhere when a user is logged in

--- apps/platform/src/app/[locale]/(wired-pages)/courses/[slug]/layout.tsx
+++ apps/platform/src/app/[locale]/(wired-pages)/courses/[slug]/layout.tsx
@@ -1,17 +1,8 @@
-import SidebarLayout from '../../../../../lib/infrastructure/server/pages/layouts/sidebar-layout';
-import { TLocale } from '@maany_shr/e-class-translations';
-
 export default async function CourseLayout({
     children,
-    params: paramsPromise,
 }: {
     children: React.ReactNode;
     params: Promise<{ locale: string }>;
 }) {
-    const params = await paramsPromise;
-    return (
-        <SidebarLayout params={{ ...params, locale: params.locale as TLocale }}>
-            {children}
-        </SidebarLayout>
-    );
+    return <>{children}</>;
 }
